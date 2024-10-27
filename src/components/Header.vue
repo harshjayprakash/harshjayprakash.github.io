93bc33d1067dddb011b9b0c8b00edb89b5bae25e
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-10-27 21:45:14 +0000

    Update header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,62 @@
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent, ref, type Ref } from 'vue';
+import Navigation from './Navigation.vue';
 
+export default defineComponent({
+    components: { Navigation },
+    setup() {
+        const desktopMode: Ref<boolean> = ref(window.innerWidth > 800);
+        const showNavigation: Ref<boolean> = ref(desktopMode.value);
+
+        window.addEventListener('resize', (): void => {
+            desktopMode.value = (window.innerWidth > 800);
+        });
+
+        return { desktopMode, showNavigation };
+    },
+    methods: {
+        onMenuButtonClick(_event: MouseEvent) {
+            this.showNavigation = !this.showNavigation;
+        },
+        hideShowNavigation() {
+            this.showNavigation = false;
+        }
+    }
+    
+});
 </script>
 
 <template>
-    <p>Header</p>
+    <header class="header">
+        <button 
+            class="menu-button" @click="onMenuButtonClick" 
+            v-if="!desktopMode"
+        >
+            &#x2630;
+        </button>
+        <span class="title">Harsh.</span>
+        <span class="spacer"></span>
+        <Navigation v-if="desktopMode" @linkClick="hideShowNavigation" />
+    </header>
+    <Navigation v-if="!desktopMode && showNavigation" @linkClick="hideShowNavigation" />
 </template>
 
 <style lang="css" scoped>
+.header {
+    padding: 1rem;
+    display: flex;
+}
 
+.header > * {
+    margin-right: 0.5rem;
+}
+
+.header > .menu-button {
+    border: 0;
+    background-color: inherit;
+}
+
+.header > .spacer {
+    flex-grow: 1;
+}
 </style>
