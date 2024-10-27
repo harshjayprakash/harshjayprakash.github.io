3cfc52309f7fa151ffbad43fb532a05b7836a68b
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-10-27 17:42:16 +0000

    Refactor App to Main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './main.fonts'
-import { createApp } from 'vue';
-import App from './App.vue';
+import { createApp, type App } from 'vue';
+import Main from './Main.vue';
 
-const app: App<Element> = createApp(App);
+const app: App<Element> = createApp(Main);
 
 app.mount('#app');
