60110fd18937616f0ea404cc81c41db9bfecb299
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-10-27 22:01:27 +0000

    Update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,24 @@
 <template>
-
+    <footer class="footer">
+        <p>Copyright &copy; {{ copyrightYear }}. Harsh Jayprakash</p>
+        <p>Handcrafted with Vue</p>
+    </footer>
 </template>
 
 <script lang="ts">
+import { defineComponent } from 'vue';
 
+export default defineComponent({
+    setup() {
+        const copyrightYear: string = 'MMXXIV';
+
+        return { copyrightYear };
+    },
+});
 </script>
 
 <style lang="css" scoped>
-
+.footer {
+    padding: 1rem;
+}
 </style>
