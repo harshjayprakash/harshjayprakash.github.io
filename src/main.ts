79b3c3c62696517104bcb052ea12af9c17e10902
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-10-27 18:21:03 +0000

    Use router

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import './assets/style/main.css';
 import './main.fonts';
 import { createApp, type App } from 'vue';
 import Main from './Main.vue';
+import router from './router';
 
 const app: App<Element> = createApp(Main);
+
+app.use(router)
 
 app.mount('#app');
