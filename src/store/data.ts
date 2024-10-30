9568c591e4361ead50b7ecfd449afebb706e0962
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-10-30 19:47:55 +0000

    Create data store

---
+++ src/store/data.ts
@@ -0,0 +1,6 @@
+import type { DevProject } from "./data.type";
+
+export const devProjects: DevProject[] = [
+
+];
+
---
+++ src/store/data.type.ts
@@ -0,0 +1,17 @@
+export interface DevProject {
+    name: string;
+    description: string;
+    timeframe: string;
+    category: string;
+    technology: string;
+    graphic: string;
+    url: string;
+    gitLink: string;
+};
+
+export interface ArtProject {
+    name: string;
+    description: string;
+    url: string;
+};
+
