61040f7fc5f8f6a08821871cc51fdf61e999ef51
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-10-30 19:47:42 +0000

    Update routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,28 +1,39 @@
 import { createRouter, createWebHistory, type Router } from "vue-router";
 
-
 const router: Router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path: '/',
-            redirect: '/home',
+            path: '/', redirect: '/home',
         },
         {
-            path: '/home',
-            name: 'Home',
+            path: '/home', name: 'Home',
             component: () => import('./../views/Home.vue'),
         },
         {
-            path: '/about',
-            name: 'About',
+            path: '/about', name: 'About',
             component: () => import('./../views/About.vue'),
         },
         {
-            path: '/portfolio/dev',
-            name: 'Developer Portfolio',
+            path: '/contact', name: 'Contact',
+            component: () => import('./../views/Contact.vue'),
+        },
+        {
+            path: '/brand', name: 'Branding',
+            component: () => import('./../views/Brand.vue'),
+        },
+        {
+            path: '/portfolio/art', name: 'Art Portfolio',
+            component: () => import('./../views/Portfolio/Art/Index.vue'),
+        },
+        {
+            path: '/portfolio/dev', name: 'Developer Portfolio',
             component: () => import('./../views/Portfolio/Dev/Index.vue'),
-        }
+        },
+        {
+            path: '/portfolio/photography', name: 'Photography Portfolio',
+            component: () => import('./../views/Portfolio/Photography/Index.vue'),
+        },
     ]
 });
 
