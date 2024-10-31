3b056a92336790576864b32eda5aa85fb833aa30
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-10-31 13:43:52 +0000

    Add link to source

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,6 +2,7 @@
     <footer class="footer">
         <p>Copyright &copy; {{ copyrightYear }}. Harsh Jayprakash</p>
         <p>Handcrafted with Vue</p>
+        <a href="https://github.com/harshjayprakash/personal-portfolio">View Source Code &UpperRightArrow;</a>
     </footer>
 </template>
 
