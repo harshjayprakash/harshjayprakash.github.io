2aaf5e027857820ddc1622f1a01c88276c66af54
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-11-01 17:35:50 +0000

    Update base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: 'https://harshjayprakash.github.io/personal-portfolio/',
+    base: '/personal-portfolio/',
     plugins: [
         vue(),
     ],
