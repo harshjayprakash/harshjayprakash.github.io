2911edd76da6157073e5f16d151ffd0066c04080
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-11-02 11:59:09 +0000

    Add build options

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +13,12 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: resolve(__dirname, 'index.html')
         }
     }
 })
