0efe6476e08b5717e348b8d77f981b8559fa412d
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-11-16 22:21:51 +0000

    Add blog routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,23 @@
+import CardComponent from "@/components/ui/CardComponent.vue";
+import { blogPostList } from "@/store/data/blogPostList";
 import AboutPage from "@/views/AboutPage.vue";
 import BlogPage from "@/views/Blog/BlogPage.vue";
 import HomePage from "@/views/HomePage.vue";
+import NotFoundPage from "@/views/NotFoundPage.vue";
 import DeveloperPortfolioPage from "@/views/Portfolio/DeveloperPortfolioPage.vue";
 import { createRouter, createWebHistory, type Router, type RouteRecordRaw } from "vue-router";
+
+function createBlogRoutes(): RouteRecordRaw[] {
+    let routes: RouteRecordRaw[] = [];
+    blogPostList.forEach(post => {
+        routes.push({
+            path: post.uri.toString(),
+            name: post.name.toString(),
+            component: post.content ?? CardComponent,
+        });
+    });
+    return routes;
+}
 
 const router: Router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,6 +27,13 @@ const router: Router = createRouter({
         { path: '/about', name: 'About', component: AboutPage },
         { path: '/blog', name: 'Blog', component: BlogPage },
         { path: '/portfolio/dev', name: 'Developer Portfolio', component: DeveloperPortfolioPage },
+        {
+            path: '/blog/post',
+            name: '',
+            component: null,
+            children: createBlogRoutes(),
+        },
+        { path: '/:catchAll(.*)*', name: 'Not Found', component: NotFoundPage }
     ],
 });
 
