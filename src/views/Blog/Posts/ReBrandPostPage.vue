aa692d83f83e06aa1112de5c5d86dcd74b424636
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-11-16 22:22:59 +0000

    Create rebrand post page

---
+++ src/views/Blog/Posts/ReBrandPostPage.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="blog-post">
+        <h1 class="blog-post__title">Rebrand.</h1>
+        <div class="blog-post__content">
+            <p>A rebrand serves as redefining how.</p>
+        </div>
+    </section>
+</template>
+
+<script lang="ts">
+import OctogonComponent from '@/components/ui/OctogonComponent.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'ReBrandPostPage',
+    components: { OctogonComponent },
+});
+</script>
+
+<style lang="css" scoped>
+.blog-post__image {
+    padding: 1rem;
+}
+
+.blog-post__content {
+    max-width: 80ch;
+}
+</style>
