9e97172e2edd432845fb470a5c9aeebd19539be8
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-11-16 22:22:27 +0000

    Import components. Update template

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,12 +1,22 @@
 <template>
-    About
+    <section class="about">
+        <AboutQualificationsSection />
+    </section>
 </template>
 
 <script lang="ts">
+import AboutExperienceSection from '@/components/sections/About/AboutExperienceSection.vue';
+import AboutQualificationsSection from '@/components/sections/About/AboutQualificationsSection.vue';
+import AboutSkillsSection from '@/components/sections/About/AboutSkillsSection.vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
     name: 'AboutPage',
+    components: { 
+        AboutQualificationsSection, 
+        AboutSkillsSection, 
+        AboutExperienceSection 
+    },
     setup() { },
 });
 </script>
