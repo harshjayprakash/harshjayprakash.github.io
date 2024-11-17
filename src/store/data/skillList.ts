784feab5a46a2138cc8d896cc45ad89e2fc02ee6
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-11-17 22:38:47 +0000

    Add skills

---
+++ src/store/data/skillList.ts
@@ -0,0 +1,23 @@
+export const traditionalSkills: String[] = [
+    'Attention-to-detail',
+    'Time Management',
+    'Problem Solving',
+    'Work Ethic',
+    'Adaptable',
+    'Resilient'
+];
+
+export const technicalSkills: String[] = [
+    'Angular',
+    'C Programming Language',
+    'HTML + CSS',
+    'GNU+Linux',
+    'Java',
+    'Microsoft Office',
+    'Microsoft Windows',
+    'Pascal',
+    'Python',
+    'Visual Basic .NET',
+    'VMware Workstation',
+    'Vue.js'
+]
