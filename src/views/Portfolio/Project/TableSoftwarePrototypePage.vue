e64b20ff42f947f1bce45acbf3b56d6dfeddfbea
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-11-17 22:58:21 +0000

    Create developer project pages

---
+++ src/views/Portfolio/Project/TableSoftwarePrototypePage.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'TableSoftwarePrototypePage'
+});
+</script>
+
+<style lang="css" scoped>
+
+</style>
