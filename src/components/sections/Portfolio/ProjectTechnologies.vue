9e0ee49c5acbd9b74afb774fc21a9ffe0c999b85
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-11-18 16:04:31 +0000

    Create project based components

---
+++ src/components/sections/Portfolio/ProjectTechnologies.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import type { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'ProjectTechnologiesSection',
+});
+</script>
+
+<style lang="css" scoped>
+
+</style>
