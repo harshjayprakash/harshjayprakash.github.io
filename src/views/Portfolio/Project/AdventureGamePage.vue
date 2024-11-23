712ebfec9b42c517ba4c186ac863b2eefca3e7ca
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-11-23 12:41:01 +0000

    Add content

--- src/views/Portfolio/Project/AdventureGamePage.vue
+++ src/views/Portfolio/Project/AdventureGamePage.vue
@@ -1,5 +1,23 @@
 <template>
-
+    <section class="project-content">
+        <h2>Overview</h2>
+        <p>
+            A project originally started as a college homework assignment for a single
+            week, which involved making an adventure game. Original requirements involved:
+        </p>
+        <ul>
+            <li>The user must make at least three choices.</li>
+            <li>Each choice must have a conclusion for each path.</li>
+            <li>Make use of a random element.</li>
+            <li>Free to add any additional features.</li>
+        </ul>
+        <h2>Purpose</h2>
+        <p>
+            This project was to help continue to develop my programming knowledg. At the
+            time, my knowledge was very limited to subroutines in Visual Basic .NET in
+            single module files.
+        </p>
+    </section>
 </template>
 
 <script lang="ts">
@@ -11,5 +29,7 @@ export default defineComponent({
 </script>
 
 <style lang="css" scoped>
-
+.project-content {
+    padding: 1rem 0;
+}
 </style>
