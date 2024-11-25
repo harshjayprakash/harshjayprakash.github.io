47477bd22aabaf6e3823d3492a001bb0a3a8f040
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-11-25 21:14:03 +0000

    Update font import

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
+import '@fontsource-variable/inter';
 import './assets/style/main.css';
-import './main.fonts';
 import { createApp, type App } from 'vue';
 import Main from './Main.vue';
 import router from './router';
