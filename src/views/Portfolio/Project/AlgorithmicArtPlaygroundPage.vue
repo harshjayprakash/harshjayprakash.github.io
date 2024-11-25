6f838b84da8fd2c8c1b84a34deb5cb694b33e78f
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-11-25 21:20:45 +0000

    Add article base

--- src/views/Portfolio/Project/AlgorithmicArtPlaygroundPage.vue
+++ src/views/Portfolio/Project/AlgorithmicArtPlaygroundPage.vue
@@ -1,12 +1,19 @@
 <template>
-
+    <ArticleComponent>
+        <br/>
+        <h2>Overview</h2>
+        <p>A project part of my Undergraduate Computer Science Degree</p>
+        <h2>Purpose</h2>
+    </ArticleComponent>
 </template>
 
 <script lang="ts">
+import ArticleComponent from '@/components/ui/ArticleComponent.vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'AlgorithmicArtPlaygroundPage'
+    name: 'AlgorithmicArtPlaygroundPage',
+    components: { ArticleComponent },
 });
 </script>
 
