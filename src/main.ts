3f516c7aa481d00c9fe2bef4a6bd4055bfaa5145
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-10 13:44:13 +0000

    Update font imports

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import '@fontsource-variable/noto-sans/wght.css';
+import '@fontsource-variable/overpass/wght.css';
 import './assets/style/main.css';
 import { createApp, type App } from 'vue';
 import Main from './Main.vue';
