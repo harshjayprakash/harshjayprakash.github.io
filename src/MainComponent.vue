8861e9adc643bfc0a8406a50ee746407a105a61d
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-01-03 12:33:50 +0000

    Add footer component

--- src/MainComponent.vue
+++ src/MainComponent.vue
@@ -1,16 +1,18 @@
 <template>
     <HeaderComponent />
     <RouterView />
+    <FooterComponent />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { RouterView } from 'vue-router';
 import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
 
 export default defineComponent({
     name: 'MainComponent',
-    components: { RouterView, HeaderComponent },
+    components: { RouterView, HeaderComponent, FooterComponent },
 });
 </script>
 
