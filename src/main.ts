126832abcdf870061d5bd13e0c758a4f9d465f03
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-01-03 12:09:53 +0000

    Use router

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { type App, createApp } from 'vue';
 
 import './assets/style/main.css'
 import MainComponent from './MainComponent.vue';
+import router from './router';
 
 const app: App<Element> = createApp(MainComponent);
+app.use(router);
 app.mount('#app');
