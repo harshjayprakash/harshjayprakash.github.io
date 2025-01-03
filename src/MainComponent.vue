3456722d897a517a7d310549c945f98684d1a120
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-01-03 12:34:02 +0000

    Organise imports

--- src/MainComponent.vue
+++ src/MainComponent.vue
@@ -7,6 +7,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { RouterView } from 'vue-router';
+
 import HeaderComponent from './components/HeaderComponent.vue';
 import FooterComponent from './components/FooterComponent.vue';
 
