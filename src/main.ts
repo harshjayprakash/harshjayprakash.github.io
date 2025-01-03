76cef80f5dc697c3cadc8418f5e679cf319677c3
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-01-03 12:14:44 +0000

    Import inter opsz stylesheet

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { type App, createApp } from 'vue';
 
+import '@fontsource-variable/inter/opsz.css';
 import './assets/style/main.css'
+
 import MainComponent from './MainComponent.vue';
 import router from './router';
 
