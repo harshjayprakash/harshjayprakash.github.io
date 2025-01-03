9d4870e48cb8165d5a16b353b55006b5e881cdc1
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-01-03 12:06:31 +0000

    Create home view component

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+    Home
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'HomeView',
+});
+</script>
+
+<style lang="css" scoped>
+
+</style>
