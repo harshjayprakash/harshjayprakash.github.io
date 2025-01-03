9d235a266c56b9a8ff0973aa043f016a73ef9dc9
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-01-03 12:33:33 +0000

    Add header component

--- src/MainComponent.vue
+++ src/MainComponent.vue
@@ -1,14 +1,16 @@
 <template>
+    <HeaderComponent />
     <RouterView />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { RouterView } from 'vue-router';
+import HeaderComponent from './components/HeaderComponent.vue';
 
 export default defineComponent({
     name: 'MainComponent',
-    components: { RouterView },
+    components: { RouterView, HeaderComponent },
 });
 </script>
 
