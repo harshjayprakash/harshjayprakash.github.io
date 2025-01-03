0648fd2d28888add4920099ff4b7bb8ee415bfff
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-01-03 12:32:17 +0000

    Create header component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    Header
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'HeaderComponent',
+});
+</script>
+
+<style lang="css" scoped>
+
+</style>
