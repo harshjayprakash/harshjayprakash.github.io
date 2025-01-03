07fef1aa56a8e7b6a8f3d2b1c6972b0917681d03
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-01-03 12:32:26 +0000

    Create footer component

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    Footer
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'FooterComponent',
+});
+</script>
+
+<style lang="css" scoped>
+
+</style>
