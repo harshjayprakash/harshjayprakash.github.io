88d8173b3945c34aaf148bae3b2112f64f20d6e8
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-01-03 12:48:46 +0000

    Create developer project interface

---
+++ src/store/interface/DeveloperProject.ts
@@ -0,0 +1,9 @@
+export interface DeveloperProject {
+    name: String;
+    description: String;
+    graphic: String;
+    reference: String;
+    abbreviation: String;
+    slug: String;
+    gitUri: String;
+};
