d3b421b6aef8d1f9be2f85cfc0a119358286e155
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-01-03 12:05:20 +0000

    Import main stylesheet

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { type App, createApp } from 'vue';
 
+import './assets/style/main.css'
 import MainComponent from './MainComponent.vue';
 
 const app: App<Element> = createApp(MainComponent);
