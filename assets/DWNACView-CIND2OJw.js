32fd80053d87d67c803209b4c0e30cb7b513a58a
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-01-04 18:09:10 +0000

    Updates

---
+++ assets/DWNACView-CIND2OJw.js
@@ -0,0 +1,1 @@
+import{g as o}from"./projectRetriever-M3UHNnrV.js";import{d as r,_ as a,o as s,c,a as n,t as p}from"./index-DmakvVMV.js";const i=r({name:"DWNACView",data(){return{project:o()}}}),m={class:"project-article"};function _(e,d,l,f,u,j){var t;return s(),c("section",m,[n("h1",null,p((t=e.project)==null?void 0:t.name),1)])}const h=a(i,[["render",_]]);export{h as default};
---
+++ assets/projectRetriever-M3UHNnrV.js
@@ -0,0 +1,1 @@
+import{r as e,b as o}from"./index-DmakvVMV.js";function l(){const t=e.currentRoute.value.fullPath.slice(10,e.currentRoute.value.fullPath.length);return o.find(r=>r.slug===t)}export{l as g};
