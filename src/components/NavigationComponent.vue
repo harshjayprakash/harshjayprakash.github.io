0706cee2e7b9e5d2eefe7840f90a4c678fe0e7cd
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-01-04 13:45:42 +0000

    Get link paths from router

--- src/components/NavigationComponent.vue
+++ src/components/NavigationComponent.vue
@@ -3,10 +3,21 @@
 </template>
 
 <script lang="ts">
+import router from '@/router';
 import { defineComponent } from 'vue';
+import type { RouteRecordNormalized, RouteRecordRaw } from 'vue-router';
 
 export default defineComponent({
     name: 'NavigationComponent',
+    data() {
+        const linkFilter: String[] = ['/home'];
+        const linksToShow: RouteRecordRaw[] = router.getRoutes()
+            .filter((route: RouteRecordNormalized) =>
+                linkFilter.includes(route.path))
+            .sort((linkA: RouteRecordNormalized, linkB: RouteRecordNormalized) =>
+                linkFilter.indexOf(linkA.path) - linkFilter.indexOf(linkB.path));
+        return { linksToShow };
+    }
 });
 </script>
 
