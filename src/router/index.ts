b2ee00f5e99e5ff75636d7ee2729e06c717dc66f
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-01-04 17:18:16 +0000

    Add developer project routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,27 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router";
+
+import { developerProjects } from "@/store/data/developerProjects";
+import type { DeveloperProject } from "@/store/interface/DeveloperProject";
+
+function createDeveloperProjectRoutes(): RouteRecordRaw[] {
+    const routes: RouteRecordRaw[] = [];
+    developerProjects.forEach((project: DeveloperProject) => {
+        routes.push(
+            {
+                path: `/work/dev/${project.slug}`,
+                name: project.name.toString(),
+                component: () => import(
+                    `./../views/dev-work/DW${project.abbreviation.toUpperCase()}View.vue`),
+            }
+        )
+    })
+    return routes;
+}
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
+        ...createDeveloperProjectRoutes(),
         {
             path: '/',
             redirect: '/home'
