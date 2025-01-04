5c9dc137a40f9d9b1950db873ea558bf290d0f94
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-01-04 13:40:39 +0000

    Add navigation

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,14 +1,18 @@
 <template>
     <header>
         Header
+        <NavigationComponent />
     </header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import NavigationComponent from './NavigationComponent.vue';
+
 export default defineComponent({
     name: 'HeaderComponent',
+    components: { NavigationComponent },
 });
 </script>
 
