4a1ee222b22bec2cefb2246214fbd004693256cf
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-01-04 17:43:15 +0000

    Get project from url

--- src/views/dev-work/DWPNVView.vue
+++ src/views/dev-work/DWPNVView.vue
@@ -1,12 +1,20 @@
 <template>
-    Hi
+    <section class="project-article">
+        <h1>{{ project?.name }}</h1>
+    </section>
 </template>
 
 <script lang="ts">
+import { getProjectFromRoute } from '@/service/ProjectRetriever';
+import type { DeveloperProject } from '@/store/interface/DeveloperProject';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
     name: 'DWPNVView',
+    data() {
+        const project: DeveloperProject | undefined = getProjectFromRoute();
+        return { project };
+    }
 });
 </script>
 
