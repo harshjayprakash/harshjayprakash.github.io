659acfb75b81428152172aee86d18259638e74ea
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-01-05 14:29:27 +0000

    Updates

---
+++ assets/DWNACView-nFyKHk21.js
@@ -0,0 +1,1 @@
+import{P as e}from"./ProjectArticleComponent-Dx-9ca5O.js";import{d as t,_ as n,o as c,c as r,r as s}from"./index-nEIu5-cm.js";const p=t({name:"DWNACView",components:{ProjectArticleComponent:e}});function a(m,i,_,f,l,C){const o=s("ProjectArticleComponent");return c(),r(o)}const u=n(p,[["render",a]]);export{u as default};
--- assets/DWPNVView-ekLxztGc.js
+++
@@ -1,1 +0,0 @@
-import{g as o}from"./projectRetriever-M3UHNnrV.js";import{d as r,_ as a,o as s,c,a as n,t as p}from"./index-DmakvVMV.js";const i=r({name:"DWPNVView",data(){return{project:o()}}}),m={class:"project-article"};function _(e,d,l,f,u,j){var t;return s(),c("section",m,[n("h1",null,p((t=e.project)==null?void 0:t.name),1)])}const g=a(i,[["render",_]]);export{g as default};
