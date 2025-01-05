7327d17cc8e77769102928b1059185ac9ec9ad60
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-01-05 13:39:45 +0000

    Extract to project article component

--- src/components/ProjectArticleComponent.vue
+++ src/components/ProjectArticleComponent.vue
@@ -1,15 +1,43 @@
 <template>
-    project-article
+    <section class="project-article">
+        <div class="breadcrumb-bar">
+            <RouterLink class="link" to="/home">
+                Home
+            </RouterLink>
+            <span>></span>
+            <span>{{ project?.name }}</span>
+        </div>
+        <h1>{{ project?.name }}</h1>
+    </section>
 </template>
 
 <script lang="ts">
+import { getProjectFromRoute } from '@/service/projectRetriever';
+import type { DeveloperProject } from '@/store/interface/DeveloperProject';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'ProjectArticleComponent'
+    name: 'ProjectArticleComponent',
+    data() {
+        const project: DeveloperProject | undefined = getProjectFromRoute();
+        return { project };
+    }
 });
 </script>
 
-<style lang="css" module>
+<style lang="css" scoped>
+.breadcrumb-bar {
+    padding: 0.5rem 0;
+    display: flex;
+    gap: 0.5rem;
+}
 
+.breadcrumb-bar .link {
+    text-decoration: none;
+    color: inherit;
+}
+
+.breadcrumb-bar .link:hover {
+    background-color: var(--clr-card-container-hover);
+}
 </style>
