ede059ef1731c98b69cf4da0e0100b16377f8629
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-01-05 13:01:53 +0000

    Create project article component

---
+++ src/components/ProjectArticleComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    project-article
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'ProjectArticleComponent'
+});
+</script>
+
+<style lang="css" module>
+
+</style>
