c40637d18e7b0fc15e11a2719caa84c28b134096
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-01-12 16:36:06 +0000

    Add content

--- src/views/dev-work/DWXBKView.vue
+++ src/views/dev-work/DWXBKView.vue
@@ -3,10 +3,35 @@
     <section class="article-content">
         <h2>Overview</h2>
         <p>
-            This is a small game
+            "Experimental Blok" is a small game designed and developed using the Windows
+            API in C. While this project does not aim to solve a specific problem, it
+            offers a valuable learning experience by building fundational skills in C and
+            system-level development.
         </p>
-        <h2>The Why.</h2>
-        <h2>The How.</h2>
+        <p>
+            This game provides a top-down view of a maze. The player ("movable box") moves
+            using arrow keys on the keyboard, avoiding the walls ("obstructable boxes").
+        </p>
+        <p>
+            The user interface involves using distinct layers to encapsulate
+            functionality. This is shown in the diagram below:
+        </p>
+        <h2>The Component Model Layers</h2>
+        <img src="/img/dev/xbk-layers.png" alt="blok component layers">
+        <p>
+            The "Native Window" Layer is the base layer provided by the Windows API. This
+            is the surface where all the components are painted.
+        </p>
+        <p>
+            The "Canvas Grid" Layer provides a blank, grid system for marking boxes, which
+            the user interacts with the arrow keys. The user can click on this area to
+            create the walls.
+        </p>
+        <p>
+            The "Panel" Layer provides controls
+        </p>
+        <h2>Development History</h2>
+        <h3></h3>
     </section>
 </template>
 
