70dbb2d98cd107a0eeaba054cb7f18008ffe83e8
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-22 15:03:34 +0000

    feat(router): update routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,17 @@ import { type Router, createRouter, createWebHistory } from "vue-router";
 
 const router: Router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [],
+    routes: [
+        {
+            path: '/',
+            redirect: '/home',
+        },
+        {
+            path: '/home',
+            name: 'Home',
+            component: () => import ('./../views/HomeView.vue'),
+        }
+    ],
 });
 
 export default router;
