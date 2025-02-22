791d236535f4a03529c52be7c68731827e00ef59
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-22 15:02:16 +0000

    feat: add router view

--- src/MainComponent.vue
+++ src/MainComponent.vue
@@ -1,13 +1,17 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { RouterView } from 'vue-router';
 
 export default defineComponent({
     name: 'MainComponent',
+    components: { RouterView },
 });
 </script>
 
 <template>
     <p>Main Component</p>
+
+    <RouterView />
 </template>
 
 <style lang="css" scoped>
