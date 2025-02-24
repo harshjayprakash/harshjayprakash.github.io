2becceb943abfbb7b269b98937925f12b08d7fcc
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-24 15:31:43 +0000

    feat(ui): add navigation

---
+++ src/components/VertexNavigation.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'VertexNavigation',
+});
+</script>
+
+<template>
+    <nav>nav</nav>
+</template>
+
+<style lang="css" scoped>
+
+</style>
