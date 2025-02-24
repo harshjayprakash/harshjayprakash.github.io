253e26817b43f55ffe41405037047c92d7325bc5
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-24 13:34:53 +0000

    feat(ui): add vertex provider

---
+++ src/components/VertexProvider.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'VertexProvider',
+});
+</script>
+
+<template>
+    <div class="vertex-provider">
+        <slot />
+    </div>
+</template>
+
+<style lang="css" scoped>
+
+</style>
