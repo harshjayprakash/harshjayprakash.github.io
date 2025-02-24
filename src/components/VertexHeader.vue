d28fc9e81ee8d75f45aa2d088a4e8df98f21961a
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-24 13:48:07 +0000

    feat(ui): add header component

---
+++ src/components/VertexHeader.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'VertexHeader',
+});
+</script>
+
+<template>
+    <header>header</header>
+</template>
+
+<style lang="css" scoped>
+
+</style>
