cefe36c4badd69ab91acffc945b2fe445de7b7ed
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-24 13:58:51 +0000

    feat(ui): add header menu button and title

--- src/components/VertexHeader.vue
+++ src/components/VertexHeader.vue
@@ -7,9 +7,30 @@ export default defineComponent({
 </script>
 
 <template>
-    <header>header</header>
+    <header class="vertex-header">
+        <button class="menu-button">&#x2630;</button>
+        <span class="title">Harsh.</span>
+    </header>
 </template>
 
 <style lang="css" scoped>
+.vertex-header {
+    display: flex;
+    place-items: center;
+}
+
+.vertex-header .menu-button,
+.vertex-header .title {
+    padding: 0.8rem 1rem;
+}
+
+.vertex-header .menu-button {
+    background-color: inherit;
+    border: 0;
+}
+
+.vertex-header .menu-button:hover {
+    background-color: blue;
+}
 
 </style>
