798a24e9513df470a514e1543cae8a7ad84c977f
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-03 20:36:00 +0000

    build: optimise for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -6,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/',
     plugins: [
         vue(),
         vueDevTools(),
@@ -15,4 +17,10 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url))
         },
     },
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: resolve(__dirname, 'index.html')
+        }
+    }
 })
