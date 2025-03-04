d209fb41d2f6570bbaf0c63f4265e4bc200debf6
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-04 16:51:54 +0000

    feat(comp): add header component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const HeaderComponent = defineComponent({
+    name: 'HeaderComponent',
+});
+
+export default HeaderComponent;
+</script>
+
+<template>
+    <header>HeaderComponent</header>
+</template>
+
+<style lang="css" scoped>
+</style>
