85f62f5b81af6ba980c3148bba4580b86c7e359a
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-04 16:39:48 +0000

    feat(router): add paths

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,15 @@ import { createRouter, createWebHistory } from "vue-router";
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
-
+        {
+            path: '/',
+            redirect:  '/home',
+        },
+        {
+            path: '/home',
+            name: 'Home',
+            component: import('@/views/HomeView.vue'),
+        },
     ]
 });
 
