cda9f6efd1a10e7bddcc483f0012bf6d817debf9
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-06 18:14:34 +0000

    feat(view): update home project section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,25 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import { IDeveloperProject } from '@/store/interfaces/DeveloperProject';
+import developerProjects from '@/store/data/developerProjects';
+
 const HomeView = defineComponent({
     name: 'HomeView',
     setup() {
         const _profilePicture = 'https://avatars.githubusercontent.com/u/58461297?v=4';
+        const _devFilter = ['aap', 'xbk', 'wpq', 'dwf', 'lls', 'ccs'];
+        const _devProjects = developerProjects
+            .filter(project => project.available && project.abbreviation !== 'ppw')
+            .sort((projectA, projectB) =>
+                _devFilter.indexOf(
+                    projectA.abbreviation.toString()) - _devFilter.indexOf(
+                        projectB.abbreviation.toString())
+            );
 
         return {
             personaPicture: _profilePicture as String,
+            devProjects: _devProjects as IDeveloperProject[]
         }
     },
 });
@@ -41,6 +53,31 @@ export default HomeView;
     <hr class="divider" />
     <section class="projects">
         <h2>Projects.</h2>
+        <p>
+            Below shows a collection of my undertaken projects, ranging from web
+            development to traditional application development. Currently these cards link to their respective GitHub repositories. In future,
+            each project will have seperate pages.
+        </p>
+        <div class="project-grid">
+            <a
+                class="project-card-wrapper"
+                v-for="(project, idx) in devProjects" :key="idx"
+                :href="project.gitUri.toString()"
+            >
+                <article class="project-card">
+                    <img
+                        class="project-card__image"
+                        :src="`/img/${project.abbreviation}-screenshot.PNG`"
+                        :alt="`${project.name} screenshot`"
+                    />
+                    <small class="project-card__timetech">
+                        {{ project.timeframe }} &bullet; {{ project.technology }}
+                    </small>
+                    <h3 class="project-card__title">{{ project.name }}</h3>
+                    <p class="project-card__description">{{ project.description }}</p>
+                </article>
+            </a>
+        </div>
     </section>
 </template>
 
@@ -93,6 +130,39 @@ export default HomeView;
 }
 
 .projects {
+    margin-block: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+}
+
+.project-grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 2rem;
+}
+
+.project-card-wrapper {
+    text-decoration: none;
+    color: inherit;
+    background-color: var(--clr-surface-object);
+    padding: 1rem;
+}
+
+.project-card .project-card__image {
+    margin-block-end: 1rem;
+}
+
+.project-card .project-card__timetech {
+    opacity: 0.7;
+}
+
+@media screen and (max-width: 768px) {
+    .project-grid {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 2rem;
+}
 
 }
 </style>
