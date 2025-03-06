55593c9a63f79b7a24619ac0109ea024941969e3
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-06 18:23:44 +0000

    feat(comp): add footer component

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const FooterComponent = defineComponent({
+    name: 'FooterComponent',
+    setup() {
+        const _year = 'MMXXV';
+
+        return {
+            copyrightYear: _year as String
+        };
+    }
+});
+
+export default FooterComponent;
+</script>
+
+<template>
+    <footer class="footer">
+        <small>Copyright &copy; {{ copyrightYear }} Harsh Jayprakash.</small>
+        <small>Handcrafted with Vue.</small>
+    </footer>
+</template>
