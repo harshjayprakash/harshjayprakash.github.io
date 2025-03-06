8b540359e89fd125ca2c213482fcd4de66a62be8
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-06 16:54:02 +0000

    feat(comp): add wrapper component

---
+++ src/components/WrapperComponent.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const WrapperComponent = defineComponent({
+    name: 'WrapperComponent',
+});
+
+export default WrapperComponent;
+</script>
+
+<template>
+    <div>
+
+    </div>
+</template>
+
+<style lang="css" scoped>
+</style>
