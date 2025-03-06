bf8311270dcb80f6879b79ed2b8bce3481b7455b
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-06 15:40:05 +0000

    feat(comp): add nav component

---
+++ src/components/NavComponent.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const NavComponent = defineComponent({
+    name: 'NavComponent'
+});
+
+export default NavComponent;
+</script>
+
+<template>
+    <nav>nav</nav>
+</template>
+
+<style>
+</style>
