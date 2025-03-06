561b4f3ca4ea26c2a44dc26893488925dd35019d
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-06 16:52:35 +0000

    feat(comp): update header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,16 +1,36 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
+import NavComponent from './NavComponent.vue';
 
 const HeaderComponent = defineComponent({
     name: 'HeaderComponent',
+    components: { NavComponent },
 });
 
 export default HeaderComponent;
 </script>
 
 <template>
-    <header>HeaderComponent</header>
+    <header class="header">
+        <span class="title">Harsh.</span>
+        <hr class="vbar" aria-hidden="true" />
+        <NavComponent />
+    </header>
 </template>
 
 <style lang="css" scoped>
+.header {
+    background-color: var(--clr-surface-object);
+    position: sticky;
+    top: 0;
+    padding-block: 1rem;
+    display: flex;
+}
+
+.header .vbar {
+    border: 0;
+    border-inline-end: 0.1rem solid var(--clr-on-surface-neutral);
+    margin-inline: 1rem;
+    opacity: 0.4;
+}
 </style>
