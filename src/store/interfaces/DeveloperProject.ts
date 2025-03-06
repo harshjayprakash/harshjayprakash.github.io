a5953d68cd97b9706932328e489de4dbceb1e176
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-06 18:14:10 +0000

    feat(store): add project data

---
+++ src/store/interfaces/DeveloperProject.ts
@@ -0,0 +1,14 @@
+interface IDeveloperProject {
+    name: String;
+    description: String;
+    reference: String;
+    timeframe: String;
+    status: String;
+    available: Boolean;
+    technology: String;
+    abbreviation: String;
+    slug: String;
+    gitUri: String;
+};
+
+export type { IDeveloperProject };
