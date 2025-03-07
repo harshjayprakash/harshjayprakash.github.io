dd818fd2a3664e5023efa7e695cf2f36cca95185
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-07 17:59:22 +0000

    feat(comp): update footer content and styles

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -17,7 +17,42 @@ export default FooterComponent;
 
 <template>
     <footer class="footer">
-        <small>Copyright &copy; {{ copyrightYear }} Harsh Jayprakash.</small>
-        <small>Handcrafted with Vue.</small>
+        <small>Copyright &copy; {{ copyrightYear }} Harsh Jayprakash. </small>
+        <small>Handcrafted with Vue. Under construction.</small>
+        <hr class="divider" />
+        <div class="links">
+            <a>Source Code</a>
+            <hr class="vbar" />
+            <a>Uses</a>
+            <hr class="vbar" />
+            <a>License</a>
+            <hr class="vbar" />
+            <a>Privacy</a>
+        </div>
     </footer>
 </template>
+
+<style lang="css" scoped>
+.footer {
+    padding-block: 1rem;
+}
+
+.footer .links {
+    display: flex;
+    margin-block-start: 0.5rem;
+}
+
+.divider {
+    border: 0;
+    border-block-end: 0.1rem solid var(--clr-on-surface-neutral);
+    opacity: 0.2;
+    margin-block: 1rem;
+}
+
+.vbar {
+    border: 0;
+    border-inline-end: 0.1rem solid var(--clr-on-surface-neutral);
+    margin-inline: 1rem;
+    opacity: 0.2;
+}
+</style>
