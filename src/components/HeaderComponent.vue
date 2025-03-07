ab703db48e196815054949925e52a39d88a28cda
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-07 17:58:49 +0000

    feat(comp): update header to be floating

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -20,11 +20,17 @@ export default HeaderComponent;
 
 <style lang="css" scoped>
 .header {
-    background-color: inherit;
+    background-color: var(--clr-surface-object);
     position: sticky;
-    top: 0;
-    padding-block: 1rem;
+    top: 1rem;
     display: flex;
+    width: fit-content;
+    padding-block: 0.75rem;
+    padding-inline: 1rem;
+    box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+    border-radius: 0.15rem;
+    z-index: 10;
+    border: 1px solid rgb(0 0 0 /0.2);
 }
 
 .header .vbar {
