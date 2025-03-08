564cd61fdf703904f5a535474e57387c633ecf9e
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-08 11:15:02 +0000

    feat(comp-card): add card component

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const CardComponent = defineComponent({
+    name: 'CardComponent',
+});
+
+export default CardComponent;
+</script>
+
+<template>
+    <article>card component</article>
+</template>
+
+<style lang="css" scoped>
+</style>
