6fe90f0866bae02aab2856ba5b8ad221bd61a962
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-08 10:28:36 +0000

    feat(view-alpha): add alpha view component

---
+++ src/views/alpha/AlphaView.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const AlphaView = defineComponent({
+    name: 'AlphaView',
+});
+
+export default AlphaView;
+</script>
+
+<template>
+    <p>Alpha View</p>
+</template>
+
+<style lang="css" scoped>
+</style>
