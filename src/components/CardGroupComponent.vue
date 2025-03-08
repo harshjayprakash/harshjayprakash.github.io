6151b566554153c3f0e2115f156fb50ec27c1e99
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-08 12:56:57 +0000

    feat(comp-card): add card group component

---
+++ src/components/CardGroupComponent.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const CardGroupComponent = defineComponent({
+    name: 'CardGroupComponent',
+});
+
+export default CardGroupComponent;
+</script>
+
+<template>
+    <div></div>
+</template>
+
+<style lang="css" scoped>
+</style>
