f682817ee253faa4a8cec344522ccf722ade4dd2
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-08 13:54:19 +0000

    feat(comp-button): add button component

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const ButtonComponent = defineComponent({
+    name: 'ButtonComponent',
+});
+
+export default ButtonComponent;
+</script>
+
+<template>
+    <button>
+        <slot></slot>
+    </button>
+</template>
+
+<style lang="css" scoped>
+</style>
