9bd6e080ededa8c3e378c389086614bab22d818c
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-08 10:59:12 +0000

    feat(comp-divider): add divider component

---
+++ src/components/DividerComponent.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const DividerComponent = defineComponent({
+    name: 'DividerComponent',
+});
+
+export default DividerComponent;
+</script>
+
+<template>
+    <hr class="v-divider" />
+</template>
+
+<style lang="css" scoped>
+</style>
