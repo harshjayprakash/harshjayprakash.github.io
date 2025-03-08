924bbcbd93b71c404286691df09c7cac958e2125
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-08 11:16:54 +0000

    feat(view-home): include home sectional components

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,15 +1,28 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
+import HomeHeroSection from './HomeHeroSection.vue';
+import HomeServicesSection from './HomeServicesSection.vue';
+import HomeProjectsSection from './HomeProjectsSection.vue';
+import HomeContactSection from './HomeContactSection.vue';
 
 const HomeView = defineComponent({
     name: 'HomeView',
+    components: {
+        HomeHeroSection,
+        HomeServicesSection,
+        HomeProjectsSection,
+        HomeContactSection
+    },
 });
 
 export default HomeView;
 </script>
 
 <template>
-    <p>Home View</p>
+    <HomeHeroSection />
+    <HomeServicesSection />
+    <HomeProjectsSection />
+    <HomeContactSection />
 </template>
 
 <style lang="css" scoped>
