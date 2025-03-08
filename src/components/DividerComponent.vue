ed9bc0ecb6b989d3c6daada3e2a4654f793eae55
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-08 11:13:29 +0000

    feat(comp-divider): update setup, props, template, style

--- src/components/DividerComponent.vue
+++ src/components/DividerComponent.vue
@@ -1,16 +1,56 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, type PropType } from 'vue';
 
 const DividerComponent = defineComponent({
     name: 'DividerComponent',
+    props: {
+        variant: {
+            type: String as PropType<'vertical' | 'horizontal'>,
+            default: 'horizontal',
+        },
+        vheight: {
+            type: String as PropType<String>,
+            default: 'auto',
+        },
+        thickness: {
+            type: String as PropType<String>,
+            default: '0.1rem',
+        }
+    },
+    setup(props) {
+
+        return {
+            direction: props.variant as String,
+            vHeight: props.vheight as String,
+            wThickness: props.thickness as String,
+        }
+    }
 });
 
 export default DividerComponent;
 </script>
 
 <template>
-    <hr class="v-divider" />
+    <hr :class="{
+        'v-divider' : direction === 'vertical',
+        'h-divider' : direction === 'horizontal',
+    }" />
 </template>
 
 <style lang="css" scoped>
+.h-divider,
+.v-divider {
+    border: 0;
+}
+
+.h-divider {
+    border-block-end: v-bind(wThickness) solid grey;
+    margin-block: 1rem;
+}
+
+.v-divider {
+    block-size: v-bind(vHeight);
+    border-inline-end: v-bind(wThickness) solid black;
+    margin-inline: 1rem;
+}
 </style>
