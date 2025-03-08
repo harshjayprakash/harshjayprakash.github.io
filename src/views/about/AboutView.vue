dfb71aa90a23ace8bf96a0e6f54ca6d2829985d3
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-08 10:21:00 +0000

    feat(view-about): add new about view

---
+++ src/views/about/AboutView.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const AboutView = defineComponent({
+    name: 'AboutView',
+});
+
+export default AboutView;
+</script>
+
+<template>
+    <p>About View</p>
+</template>
+
+<style lang="css" scoped>
+</style>
