978c9bb6a3b063467e7d91393ddf5cc2f91b1314
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-08 12:40:14 +0000

    feat(comp-tag): add tag group component

---
+++ src/components/TagGroupComponent.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const TagGroupComponent = defineComponent({
+    name: 'TagGroupComponent',
+});
+
+export default TagGroupComponent;
+</script>
+
+<template>
+    <div></div>
+</template>
+
+<style lang="css" scoped>
+</style>
