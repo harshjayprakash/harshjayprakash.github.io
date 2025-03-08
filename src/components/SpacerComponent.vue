8b461260b3f7646f7ab63990b775a9ccb53f4206
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-08 12:19:19 +0000

    feat(comp-spacer): update spacer props, setup, style

--- src/components/SpacerComponent.vue
+++ src/components/SpacerComponent.vue
@@ -1,8 +1,20 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, type PropType } from 'vue';
 
 const SpacerComponent = defineComponent({
     name: 'SpacerComponent',
+    props: {
+        space: {
+            type: String as PropType<String>,
+            default: '1rem',
+        },
+    },
+    setup(props) {
+
+        return {
+            blockSpace: props.space as String,
+        }
+    }
 });
 
 export default SpacerComponent;
@@ -13,4 +25,8 @@ export default SpacerComponent;
 </template>
 
 <style lang="css" scoped>
+.spacer {
+    padding-block: 1rem;
+    padding-block: v-bind(blockSpace);
+}
 </style>
