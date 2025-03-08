607dfbe90e0a60d432036938033e166b0f9c999c
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-08 12:54:56 +0000

    feat(comp-card): update card component props, setup, template

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,16 +1,64 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, type PropType } from 'vue';
+import { RouterLink } from 'vue-router';
 
 const CardComponent = defineComponent({
     name: 'CardComponent',
+    components: { RouterLink },
+    props: {
+        variant: {
+            type: String as PropType<'standard' | 'clickable'>,
+            default: 'standard',
+        },
+        path: {
+            type: String as PropType<String>,
+            default: '#',
+        },
+        pathType: {
+            type: String as PropType<'internal' | 'external'>,
+            default: 'internal'
+        }
+    },
+    setup(props) {
+
+        return {
+            cardType: props.variant as String,
+            link: props.path as String,
+            linkType: props.pathType as String,
+        };
+    },
 });
 
 export default CardComponent;
 </script>
 
 <template>
-    <article>card component</article>
+    <RouterLink
+        v-if="cardType === 'clickable' && path === 'internal'"
+        :to="link.toStirng()" exact class="card-link-wrapper"
+    >
+        <article class="card">
+            <slot></slot>
+        </article>
+    </RouterLink>
+    <a
+        v-if="cardType === 'clickable' && path === 'external'"
+        :href="link.toString()" class="card-link-wrapper"
+    >
+        <article class="card">
+            <slot></slot>
+        </article>
+    </a>
+    <article
+        v-if="cardType === 'standard'"
+        class="card"
+    >
+        <slot></slot>
+    </article>
 </template>
 
 <style lang="css" scoped>
+.card {
+
+}
 </style>
