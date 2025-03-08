14f216a2d84d50a027efbbb098f1a1ce94d7b7a3
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-08 10:28:22 +0000

    feat(view-home): add home contact section component

---
+++ src/views/home/HomeContactSection.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const HomeContactSection = defineComponent({
+    name: 'HomeContactSection',
+});
+
+export default HomeContactSection;
+</script>
+
+<template>
+    <p>Home Contact Section</p>
+</template>
+
+<style lang="css" scoped>
+</style>
