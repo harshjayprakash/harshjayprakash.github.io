6d8537c26da1b1ec54dbab80be53df11c841aa91
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-08 10:09:37 +0000

    feat(comp): add separator component

---
+++ src/components/SeparatorComponent.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const SeparatorComponent = defineComponent({
+    name: 'SeparatorComponent',
+});
+
+export default SeparatorComponent;
+</script>
+
+<template>
+    <div class="separator"></div>
+</template>
+
+<style lang="css" scoped>
+.separator {
+    padding: 1rem;
+}
+</style>
