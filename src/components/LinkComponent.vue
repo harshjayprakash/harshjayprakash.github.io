9df2a91c057ad1508d30f363f92c711b359674c3
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-08 10:57:26 +0000

    feat(comp-link): update link setup, props, style

--- src/components/LinkComponent.vue
+++ src/components/LinkComponent.vue
@@ -1,16 +1,45 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, type PropType } from 'vue';
 
 const LinkComponent = defineComponent({
     name: 'LinkComponent',
+    props: {
+        variant: {
+            type: String as PropType<'internal' | 'external'>,
+            default: 'internal',
+        },
+        path: {
+            type: String as PropType<String>,
+            default: '#',
+        }
+    },
+    setup(props) {
+        return {
+            within: props.variant as String,
+            link: props.path as String,
+        };
+    }
 });
 
 export default LinkComponent;
 </script>
 
 <template>
-    <a>Link Component</a>
+    <a class="link" v-if="within === 'external'" :href="link.toString()">
+        <slot></slot>
+    </a>
+    <RouterLink class="link" v-if="within === 'internal'" :to="link.toString()">
+        <slot></slot>
+    </RouterLink>
 </template>
 
 <style lang="css" scoped>
+.link {
+    text-decoration: none;
+    color: inherit;
+}
+
+.link:hover {
+    text-decoration: solid underline 0.1rem black;
+}
 </style>
