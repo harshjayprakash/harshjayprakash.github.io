1ace2a77149ac70a9d6c91c232ab1ce9fdd2f4de
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-08 10:38:22 +0000

    feat(comp-link): add link component

---
+++ src/components/LinkComponent.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const LinkComponent = defineComponent({
+    name: 'LinkComponent',
+});
+
+export default LinkComponent;
+</script>
+
+<template>
+    <a>Link Component</a>
+</template>
+
+<style lang="css" scoped>
+</style>
