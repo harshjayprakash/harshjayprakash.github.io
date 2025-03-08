c52166fb82a090d1451ff70517cf945f50caeff2
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-08 10:21:46 +0000

    feat(view-home): add home services section component

---
+++ src/views/home/HomeServicesSection.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const HomeServicesSection = defineComponent({
+    name: 'HomeServicesSection',
+});
+
+export default HomeServicesSection;
+</script>
+
+<template>
+    <p>Home Services Section</p>
+</template>
+
+<style lang="css" scoped>
+</style>
