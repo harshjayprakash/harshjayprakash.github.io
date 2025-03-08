780b661ec3c77f48734b7288045f42271742b2ea
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-08 12:30:56 +0000

    feat(comp-tag): add tag component

---
+++ src/components/TagComponent.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const TagComponent = defineComponent({
+    name: 'TagComponent',
+});
+
+export default TagComponent;
+</script>
+
+<template>
+    <span>Tag Component</span>
+</template>
+
+<style lang="css" scoped>
+</style>
