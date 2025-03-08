49797c669625524b464e447322f10200e988bab6
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-08 10:21:23 +0000

    feat(view-home): add home hero section component

---
+++ src/views/home/HomeHeroSection.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const HomeHeroSection = defineComponent({
+    name: 'HomeHeroSection',
+});
+
+export default HomeHeroSection;
+</script>
+
+<template>
+    <p>Home Hero</p>
+</template>
+
+<style lang="css" scoped>
+</style>
