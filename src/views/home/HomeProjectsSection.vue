0f700d8cf3062dd34155a412aae3a08b01b98d11
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-08 10:22:07 +0000

    feat(view-home): add home projects section component

---
+++ src/views/home/HomeProjectsSection.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const HomeProjectsSection = defineComponent({
+    name: 'HomeProjectsSection',
+});
+
+export default HomeProjectsSection;
+</script>
+
+<template>
+    <p>Home Projects Section</p>
+</template>
+
+<style lang="css" scoped>
+</style>
