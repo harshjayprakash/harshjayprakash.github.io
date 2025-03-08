4aa1dd9c3e9d67162461e453446910838eb4961f
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-08 11:32:03 +0000

    feat(comp-spacer): add spacer component

---
+++ src/components/SpacerComponent.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const SpacerComponent = defineComponent({
+    name: 'SpacerComponent',
+});
+
+export default SpacerComponent;
+</script>
+
+<template>
+    <div class="spacer"></div>
+</template>
+
+<style lang="css" scoped>
+</style>
