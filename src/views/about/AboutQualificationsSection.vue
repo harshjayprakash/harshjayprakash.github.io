88f645833027b75a6193c0c866eeffd8e32692ca
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-10 21:24:03 +0000

    feat(view-about): add about quals section

---
+++ src/views/about/AboutQualificationsSection.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const AboutQualificationsSection = defineComponent({
+    name: 'AboutQualificationsSection',
+});
+
+export default AboutQualificationsSection;
+</script>
+
+<template>
+    <p>About Quals Section</p>
+</template>
+
+<style lang="css" scoped>
+</style>
