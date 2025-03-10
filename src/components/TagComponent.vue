620ed2cb49d8b7c667e6eb42ef6ae61d6edd078d
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-10 10:48:35 +0000

    feat(comp-tag): add styles

--- src/components/TagComponent.vue
+++ src/components/TagComponent.vue
@@ -29,4 +29,17 @@ export default TagComponent;
 </template>
 
 <style lang="css" scoped>
+.tag {
+    border: 0;
+    padding-inline: 0.5rem;
+    border-radius: 0.2rem;
+}
+
+.tag:hover {
+    background-color: var(--clr-surface-container-highest);
+}
+
+.tag--active {
+    background-color: var(--clr-accent-container);
+}
 </style>
