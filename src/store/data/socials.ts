e1ae153855d96bc3fa6b7cdecb786854b0a79d05
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-10 21:02:01 +0000

    feat(store): add socials list

---
+++ src/store/data/socials.ts
@@ -0,0 +1,16 @@
+import type { ISocial } from "../interfaces/Social";
+
+const socials: ISocial[] = [
+    {
+        name: 'LinkedIn',
+        tag: 'harshjayprakash7',
+        uri: 'https://www.linkedin.com/in/harshjayprakash7/'
+    },
+    {
+        name: 'GitHub',
+        tag: 'harshjayprakash',
+        uri: 'https://github.com/harshjayprakash'
+    }
+];
+
+export default socials;
