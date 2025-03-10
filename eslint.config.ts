7a608e441fb448f7a4ffd084f29b409ad305d67e
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-10 10:34:58 +0000

    build: configure vue project

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,8 +3,8 @@ import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescri
 import oxlint from 'eslint-plugin-oxlint'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
+import { configureVueProject } from '@vue/eslint-config-typescript'
+configureVueProject({ scriptLangs: ['ts', /*'tsx' */] })
 // More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
 export default defineConfigWithVueTs(
