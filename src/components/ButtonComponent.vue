6ceb8e19756e245722292b855cdf2bb6e1bb68e9
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-10 10:37:32 +0000

    feat(comp-button): update button styles, add props, inherit attrs

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -1,18 +1,60 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, type PropType } from 'vue';
 
 const ButtonComponent = defineComponent({
     name: 'ButtonComponent',
+    inheritAttrs: false,
+    props: {
+        variant: {
+            type: String as PropType<'standard' | 'accent'>,
+            default: 'standard',
+        },
+    },
+    setup(props) {
+        return {
+            buttonType: props.variant as String,
+        };
+    }
 });
 
 export default ButtonComponent;
 </script>
 
 <template>
-    <button>
+    <button
+        v-bind="$attrs" v-on="$attrs"
+        class="button" :class="{
+            'button--standard' : buttonType === 'standard',
+            'button--accent' : buttonType === 'accent' }"
+    >
         <slot></slot>
     </button>
 </template>
 
 <style lang="css" scoped>
+.button {
+    border: 1px solid black;
+    border-radius: 0.3rem;
+    padding-inline: 0.75rem;
+}
+
+.button--standard {
+    color: var(--clr-on-surface);
+    background-color: var(--clr-surface-container-low);
+    border-color: var(--clr-outline);
+}
+
+.button--standard:hover {
+    background-color: var(--clr-surface-container);
+}
+
+.button--accent {
+    background-color: var(--clr-accent-container-highest);
+    color: var(--clr-on-accent);
+    border-color: var(--clr-accent-container);
+}
+
+.button--accent:hover {
+    background-color: var(--clr-accent-container);
+}
 </style>
