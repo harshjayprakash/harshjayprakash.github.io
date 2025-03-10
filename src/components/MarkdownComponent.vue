b0c23e039a9401e0e7e1687c4cd3145eea82a3b5
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-10 21:17:47 +0000

    feat(comp-markdown): add markdown component

---
+++ src/components/MarkdownComponent.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const MarkdownComponent = defineComponent({
+    name: 'MarkdownComponent',
+});
+
+export default MarkdownComponent;
+</script>
+
+<template>
+    <div class="markdown">
+        <slot>
+            This is some sample text.
+        </slot>
+    </div>
+</template>
+
+<style lang="css" scoped>
+</style>
