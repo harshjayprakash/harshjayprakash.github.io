9a51c4e194c2e20a06c1ee256ad6cf468eca0671
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-10 10:49:20 +0000

    feat(comp-tag): update tag group style

--- src/components/TagGroupComponent.vue
+++ src/components/TagGroupComponent.vue
@@ -9,8 +9,25 @@ export default TagGroupComponent;
 </script>
 
 <template>
-    <div></div>
+    <div class="tag-group">
+        <slot></slot>
+    </div>
 </template>
 
 <style lang="css" scoped>
+.tag-group {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    background-color: var(--clr-surface-container-lowest);
+    border-radius: 0.2rem;
+    padding: 0.5rem;
+}
+
+@media screen and (min-width: 768px) {
+    .tag-group {
+        flex-direction: row;
+        width: fit-content;
+    }
+}
 </style>
