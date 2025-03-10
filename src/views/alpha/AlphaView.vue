fcea937cf542e1ace8d62641dd1f3a7b255889dc
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-10 10:49:49 +0000

    feat(view-alpha): update

--- src/views/alpha/AlphaView.vue
+++ src/views/alpha/AlphaView.vue
@@ -1,8 +1,17 @@
 <script lang="ts">
+import ButtonComponent from '@/components/ButtonComponent.vue';
+import DividerComponent from '@/components/DividerComponent.vue';
+import LinkComponent from '@/components/LinkComponent.vue';
 import { defineComponent } from 'vue';
 
 const AlphaView = defineComponent({
     name: 'AlphaView',
+    components: { DividerComponent, ButtonComponent },
+    methods: {
+        hi() {
+            alert('hi');
+        }
+    }
 });
 
 export default AlphaView;
@@ -10,7 +19,29 @@ export default AlphaView;
 
 <template>
     <p>Alpha View</p>
+    <div class="wrapper--alpha">
+        <div class="fixed-size">
+
+            <DividerComponent variant="horizontal" />
+        </div>
+        <ButtonComponent variant="standard">
+            Standard Button
+        </ButtonComponent>
+        <br />
+        <br />
+        <ButtonComponent variant="accent">
+            Accent Button
+        </ButtonComponent>
+    </div>
 </template>
 
 <style lang="css" scoped>
+.wrapper--alpha {
+    padding: 5rem;
+}
+
+.fixed-size {
+    inline-size: 3rem;
+    block-size: 3rem;
+}
 </style>
