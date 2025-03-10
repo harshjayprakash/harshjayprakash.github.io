eeab4cf323603ac9aef7a2424da619906c702452
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-10 21:24:17 +0000

    feat(view-about): add about me section

---
+++ src/views/about/AboutMeSection.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const AboutMeSection = defineComponent({
+    name: 'AboutMeSection',
+})
+
+export default AboutMeSection;
+</script>
+
+<template>
+    <p>About Me Section</p>
+</template>
+
+<style lang="css" scoped>
+</style>
