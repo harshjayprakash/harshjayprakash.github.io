7cde5b2e9f252b07dd05b75ddfd51b54c79f9896
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-10 10:48:07 +0000

    feat(comp-tag): add is active prop, inherit attrs

--- src/components/TagComponent.vue
+++ src/components/TagComponent.vue
@@ -1,8 +1,20 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent, ref, type ComputedRef } from 'vue';
 
 const TagComponent = defineComponent({
     name: 'TagComponent',
+    inheritAttrs: false,
+    props: {
+        isActive: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    setup(props) {
+        return {
+            highlighted: computed(() => props.isActive) as ComputedRef<Boolean>,
+        }
+    }
 });
 
 export default TagComponent;
