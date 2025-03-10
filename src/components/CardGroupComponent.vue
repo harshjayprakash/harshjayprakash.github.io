fa24539e5c7fc67f415eb02ca148918e742efe93
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-10 10:40:17 +0000

    feat(comp-card): add col count prop to group

--- src/components/CardGroupComponent.vue
+++ src/components/CardGroupComponent.vue
@@ -3,6 +3,17 @@ import { defineComponent } from 'vue';
 
 const CardGroupComponent = defineComponent({
     name: 'CardGroupComponent',
+    props: {
+        desktopCols: {
+            type: Number,
+            default: 2,
+        },
+    },
+    setup(props) {
+        return {
+            cols: props.desktopCols as Number
+        };
+    }
 });
 
 export default CardGroupComponent;
