1bf0208b2fec1060ab2f02d97b8002b7c1790cd4
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-11 14:14:24 +0000

    feat(comp-badge): add badge component

---
+++ src/components/BadgeComponent.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const BadgeComponent = defineComponent({
+    name: 'BadgeComponent',
+});
+
+export default BadgeComponent;
+</script>
+
+<template>
+    <span class="badge">
+        <slot></slot>
+    </span>
+</template>
+
+<style lang="css" scoped>
+</style>
