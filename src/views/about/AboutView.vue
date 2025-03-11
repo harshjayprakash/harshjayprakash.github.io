9be38c2f4f1d7024c069c3c8d0b31937f22cdedb
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-11 14:05:25 +0000

    feat(view-about): add about sections

--- src/views/about/AboutView.vue
+++ src/views/about/AboutView.vue
@@ -1,15 +1,23 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import AboutMeSection from '@/views/about/AboutMeSection.vue';
+import AboutQualificationsSection from '@/views/about/AboutQualificationsSection.vue';
+
 const AboutView = defineComponent({
     name: 'AboutView',
+    components: {
+        AboutMeSection,
+        AboutQualificationsSection,
+    }
 });
 
 export default AboutView;
 </script>
 
 <template>
-    <p>About View</p>
+    <AboutMeSection />
+    <AboutQualificationsSection />
 </template>
 
 <style lang="css" scoped>
