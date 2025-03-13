1da1e5025e6f5cef9fe4f9bf8c925865c34a2ab2
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-13 17:01:45 +0000

    feat(comp-persona): add props

--- src/components/PersonaComponent.vue
+++ src/components/PersonaComponent.vue
@@ -3,6 +3,20 @@ import { defineComponent } from 'vue';
 
 const PersonaComponent = defineComponent({
     name: 'PersonaComponent',
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: String,
+            required: true,
+        },
+        imageUri: {
+            type: String,
+            required: true,
+        }
+    },
 });
 
 export default PersonaComponent;
