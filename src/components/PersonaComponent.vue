eb9a9569d3b9f539f96746198c3ce80374dec1b0
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-13 11:32:08 +0000

    feat(comp-persona): add persona component

---
+++ src/components/PersonaComponent.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const PersonaComponent = defineComponent({
+    name: 'PersonaComponent',
+});
+
+export default PersonaComponent;
+</script>
+
+<template>
+    <article class="persona">
+        Persona
+    </article>
+</template>
+
+<style lang="css" scoped>
+</style>
