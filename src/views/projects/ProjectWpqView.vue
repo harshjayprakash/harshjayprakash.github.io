8107c45b4952f76a3badcf3916c3ebd61a684d5f
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-13 11:44:43 +0000

    feat(view-project): add project specific pages

---
+++ src/views/projects/ProjectWpqView.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const ProjectWpqView = defineComponent({
+    name: 'ProjectWpqView',
+});
+
+export default ProjectWpqView;
+</script>
+
+<template>
+    <p>wpq</p>
+</template>
+
+<style lang="css" scoped>
+</style>
