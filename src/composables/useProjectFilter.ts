39ad6c47f5b97aa4b2d2b9f6edf9317ef9b5e838
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-17 20:17:56 +0000

    feat(composable): add use project filter function

---
+++ src/composables/useProjectFilter.ts
@@ -0,0 +1,32 @@
+import { computed, ref } from "vue"
+
+import developerProjects from "@/store/data/developerProjects"
+import type { ProjectCategory } from "@/store/interfaces/DeveloperProject";
+
+const useProjectFilter = () => {
+    const projects = ref(developerProjects);
+    const typeFilter = ref('All');
+
+    const filterByAbbreviation = (abbrevs: String[]) => {
+        projects.value = developerProjects
+            .filter(project => abbrevs.includes(project.abbreviation))
+            .sort((projectA, projectB) =>
+                abbrevs.indexOf(
+                    projectA.abbreviation) - abbrevs.indexOf(projectB.abbreviation));
+    };
+
+    const filteredProjects = computed(() => {
+        if (typeFilter.value === 'All') {
+            return projects.value;
+        }
+        return projects.value.filter(project => project.category === typeFilter.value);
+    });
+
+    const updateFilter = (category: ProjectCategory) => {
+        typeFilter.value = category;
+    }
+
+    return { filteredProjects, typeFilter, updateFilter, filterByAbbreviation };
+}
+
+export default useProjectFilter;
