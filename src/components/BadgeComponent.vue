679e3b4212565ddf4f983c8e77a7ddd43c345c20
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-18 12:32:30 +0000

    feat(comp-badge): update styles

--- src/components/BadgeComponent.vue
+++ src/components/BadgeComponent.vue
@@ -3,22 +3,56 @@ import { defineComponent } from 'vue';
 
 const BadgeComponent = defineComponent({
     name: 'BadgeComponent',
+    inheritAttrs: false,
+    props: {
+        variant: {
+            type: String,
+            default: 'filled',
+            validator(value: String) {
+                return ['filled', 'ghost', 'outline', 'tint'].includes(value.toString());
+            }
+        }
+    }
 });
 
 export default BadgeComponent;
 </script>
 
 <template>
-    <small class="badge">
+    <small
+        v-bind="$attrs" v-on="$attrs" class="badge"
+        v-bind:class="{
+            'badge--filled': $props.variant === 'filled',
+            'badge--ghost': $props.variant === 'ghost',
+            'badge--outline': $props.variant === 'outline',
+            'badge--tint': $props.variant === 'tint',
+        }"
+    >
         <slot></slot>
     </small>
 </template>
 
 <style lang="css" scoped>
-.badge {
+.badge,
+.badge--outline {
     border-radius: var(--bdr-default);
     border: 1px solid var(--clr-badge-outline);
     padding-inline: 0.3rem;
-    margin-inline: 0.5rem;
+}
+
+.badge--filled {
+    background-color: var(--clr-surface-tertiary);
+    color: var(--clr-text-inverted);
+    border-color: var(--clr-surface-tertiary);
+}
+
+.badge--ghost {
+    background-color: inherit;
+    border-color: transparent;
+}
+
+.badge--tint {
+    background-color: var(--clr-surface-glow);
+    border-color: var(--clr-outline-active);
 }
 </style>
