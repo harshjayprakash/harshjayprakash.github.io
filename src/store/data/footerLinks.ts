2e998402c6bcbca85601bd86bad28aefa723b5a4
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-24 16:15:27 +0000

    feat(store): extract footer links to separate file

---
+++ src/store/data/footerLinks.ts
@@ -0,0 +1,15 @@
+import type { ILink } from "../interfaces/Link";
+
+const footerLinks: ILink[] = [
+    {
+        variant: 'external',
+        name: 'Source',
+        path: 'https://github.com/harshjayprakash/harshjayprakash.github.io'
+    }
+];
+
+const getFooterLinks = () => {
+    return { footerLinks };
+}
+
+export default getFooterLinks;
