14833f8b5c919ac06d4ea5253fccf9fb19542dbd
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-24 16:15:57 +0000

    feat(store): extract footer version data to separate file

---
+++ src/store/data/metaData.ts
@@ -0,0 +1,10 @@
+const copyrightYear: Number = 2025;
+const versionSemver: String = '4.1';
+const versionCalver: String = '25H1E';
+const buildDate: String = '24.03.2025';
+
+const getMetaData = () => {
+    return { copyrightYear, versionSemver, versionCalver, buildDate };
+}
+
+export default getMetaData;
