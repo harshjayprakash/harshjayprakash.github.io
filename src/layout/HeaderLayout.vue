b813070b0f9fe7c4b4039f4f75a068c8c6056d7b
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-30 16:23:02 +0100

    refactor: replace with new components

---
+++ src/layout/HeaderLayout.vue
@@ -0,0 +1,91 @@
+<script setup lang="ts">
+import { computed, inject, ref } from 'vue';
+
+import NavLayout from '@/layout/NavLayout.vue';
+import BadgeWidget from '@/components/BadgeWidget.vue';
+import DividerWidget from '@/components/DividerWidget.vue';
+import SpacerWidget from '@/components/SpacerWidget.vue';
+
+const isMobile = inject('isMobile');
+const isNavVisible = ref(false);
+
+const menuButtonText = computed(() =>
+    (isNavVisible.value) ? String.fromCodePoint(10005) : String.fromCodePoint(9776)
+);
+
+const toggleNavigationVisibility = () => {
+    isNavVisible.value = !isNavVisible.value;
+}
+</script>
+
+<template>
+    <header class="header" aria-label="Site Header, Contains Navigation">
+        <button
+            v-if="isMobile"
+            class="menu-button"
+            @click="toggleNavigationVisibility"
+            aria-label="Mobile Menu Navigation Toggle Button"
+            :aria-pressed="isNavVisible"
+            tabindex="0"
+        >
+            {{ menuButtonText }}
+        </button>
+        <span class="title">Harsh.</span>
+        <BadgeWidget
+            aria-label="Website is Under Construction"
+            class="status"
+            variant="outline"
+        >
+            Under Construction
+        </BadgeWidget>
+        <DividerWidget v-if="!isMobile" variant="vertical" />
+        <NavLayout class="nav" v-if="!isMobile" />
+    </header>
+    <SpacerWidget space="1rem" />
+    <NavLayout
+        v-if="isNavVisible"
+        variant="mobile"
+        @on-link-click="toggleNavigationVisibility"
+    />
+</template>
+
+<style lang="css" scoped>
+.header {
+    --layout-header-bk: var(--clr-surface-secondary);
+    --layout-header-bk-hover: var(--clr-surface-secondary-hover);
+    --layout-header-shadow: var(--clr-shadow);
+
+    background-color: var(--layout-header-bk);
+    position: sticky;
+    top: 1rem;
+    display: flex;
+    width: fit-content;
+    padding-block: 0.8rem;
+    padding-inline: 1rem;
+    box-shadow:
+        0 0.25rem 0.375rem -0.0625rem var(--layout-header-shadow),
+        0 0.125rem 0.25rem -0.125rem var(--layout-header-shadow);
+    border-radius: var(--bdr-default);
+    z-index: 10;
+}
+
+.header .title {
+    place-self: center;
+}
+
+.header .status {
+    place-self: center;
+    margin-inline-start: 1rem;
+}
+
+.header .menu-button {
+    border: 0;
+    background-color: inherit;
+    margin-inline-end: 0.5rem;
+    padding-inline: 0.4rem;
+}
+
+.header .menu-button:hover {
+    background-color: var(--layout-header-bk-hover);
+}
+</style>
---
+++ src/layout/NavLayout.vue
@@ -0,0 +1,104 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+import router from '@/router';
+
+const props = defineProps<{
+    variant?: 'desktop' | 'mobile'
+}>();
+
+const safeVariant = computed(() => props.variant ?? 'desktop');
+
+const emits = defineEmits<{
+    onLinkClick: []
+}>();
+
+const navLinkFilter = ['/home'];
+const navLinks = router.getRoutes()
+    .filter(link => navLinkFilter.includes(link.path))
+    .sort((linkA, linkB) =>
+        navLinkFilter.indexOf(linkA.path) - navLinkFilter.indexOf(linkB.path)
+    );
+
+const isActivePage = (path: string) => {
+    return (router.currentRoute.value.fullPath === path.toString()) ?
+        'page' : '';
+}
+</script>
+
+<template>
+    <nav
+        class="nav" :class="{
+            'nav--desktop': safeVariant === 'desktop',
+            'nav--mobile' : safeVariant === 'mobile'
+        }"
+        aria-label="Site Navigation"
+    >
+        <RouterLink
+            v-for="link in navLinks"
+            class="nav-link"
+            active-class="nav-link--active" exact
+            @click="emits('onLinkClick')"
+            :key="link.path"
+            :to="link.path"
+            :aria-current="isActivePage(link.path)"
+        >
+            {{ link.name }}
+        </RouterLink>
+    </nav>
+</template>
+
+<style lang="css" scoped>
+.nav {
+    --layout-nav-border-hidden: var(--clr-outline-secondary-hidden);
+    --layout-nav-border-hover: var(--clr-outline-hover);
+    --layout-nav-border-active: var(--clr-outline-active);
+    --layout-nav-border: var(--clr-outline-secondary-hidden);
+    --layout-nav-bk: var(--clr-surface-secondary);
+    --layout-nav-shadow: var(--clr-shadow);
+
+    display: flex;
+    background-color: var(--layout-nav-bk);
+    gap: 1rem;
+}
+
+.nav .nav-link {
+    color: inherit;
+    text-decoration: none;
+}
+
+.nav .nav-link {
+    --layout-nav-border: var(--layout-nav-border-hidden);
+}
+
+.nav .nav-link:hover {
+    --layout-nav-border: var(--layout-nav-border-hover);
+}
+
+.nav .nav-link--active {
+    --layout-nav-border: var(--layout-nav-border-active);
+}
+
+.nav--desktop .nav-link {
+    border-block-end: 0.2rem solid var(--layout-nav-border);
+}
+
+.nav--mobile {
+    padding: 1rem;
+    flex-direction: column;
+    box-shadow:
+        0 0.25rem 0.375rem -0.0625rem var(--layout-nav-shadow),
+        0 0.125rem 0.25rem -0.125rem var(--layout-nav-shadow);
+    border-radius: var(--bdr-default);
+    z-index: 10;
+    margin-block-end: 1.5rem;
+    position: sticky;
+    top: 5.2rem;
+}
+
+.nav--mobile .nav-link {
+    padding-inline: 1rem;
+    border-inline-start: 0.2rem solid var(--layout-nav-border);
+}
+
+</style>
