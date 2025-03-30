d9215558f8a47c1744bcc20b9a6ef209cd5af0f6
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-30 13:03:12 +0100

    feat(main): convert to auto setup

--- src/MainComponent.vue
+++ src/MainComponent.vue
@@ -1,5 +1,5 @@
-<script lang="ts">
-import { defineComponent, provide } from 'vue';
+<script setup lang="ts">
+import { provide } from 'vue';
 import { RouterView } from 'vue-router';
 
 import FooterComponent from '@/layout/FooterComponent.vue';
@@ -8,23 +8,9 @@ import SurfaceComponent from '@/layout/SurfaceComponent.vue';
 
 import useMobileDetector from '@/composables/useMobileDetector';
 
-const MainComponent = defineComponent({
-    name: 'MainComponent',
-    components: {
-        RouterView,
-        HeaderComponent,
-        FooterComponent,
-        SurfaceComponent
-    },
-    setup() {
-        const { isMobile, windowWidth } = useMobileDetector();
-        provide('isMobile', isMobile);
-        provide('windowWidth', windowWidth);
-        return { isMobile };
-    }
-});
-
-export default MainComponent;
+const { isMobile, windowWidth } = useMobileDetector();
+provide('isMobile', isMobile);
+provide('windowWidth', windowWidth);
 </script>
 
 <template>
