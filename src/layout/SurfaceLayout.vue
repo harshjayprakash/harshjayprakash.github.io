ba33158266da6cb8f0ce92123317bd43979ed2d2
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-03-30 13:02:05 +0100

    feat(layout): add surface layout

---
+++ src/layout/SurfaceLayout.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="surface">
+        <slot name="header">No header provided.</slot>
+        <slot>No content provided.</slot>
+        <slot name="footer">No footer provided.</slot>
+    </div>
+</template>
+
+<style lang="css" scoped>
+.surface {
+    max-width: 60rem;
+    padding-inline: 2rem;
+    margin-inline: auto;
+}
+</style>
