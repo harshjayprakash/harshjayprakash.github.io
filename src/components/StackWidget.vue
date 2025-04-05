557430e324aaa020b9c73aa0be6c4389c78e69e2
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-05 21:21:30 +0100

    feat(stack): add stack component

---
+++ src/components/StackWidget.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps<{
+    spacing: number,
+    direction: 'row' | 'row-reverse' | 'column' | 'column-reverse'
+}>();
+
+const style = computed(() => {
+    return {
+        '--widget-stack-direction': `${props.direction}`,
+        '--widget-stack-spacing': `${props.spacing}rem`
+    }
+});
+</script>
+
+<template>
+    <div role="group" class="stack" :style>
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="css" scoped>
+.stack {
+    display: flex;
+    flex-direction: var(--widget-stack-direction);
+    gap: var(--widget-stack-spacing);
+}
+</style>
