443eb2ea06fca554ee8477b4e96a2aaf128a4233
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-05 21:16:30 +0100

    feat(avatar): add avatar component

---
+++ src/components/AvatarWidget.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps<{
+    variant: 'image' | 'initials',
+    imagePath: string,
+}>();
+</script>
+
+<template>
+    <img
+        v-if="props.variant === 'image'"
+        :src="props.imagePath"
+        alt="profile picture"
+        class="avatar"
+    />
+</template>
+
+<style lang="css" scoped>
+.avatar {
+    height: 5rem;
+    width: 5rem;
+}
+</style>
