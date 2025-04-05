a191dace05c89b5b359a021e988183cafb40c696
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-05 21:25:18 +0100

    refactor: update tabgroup to tablist

---
+++ src/components/TabListWidget.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { computed, provide } from 'vue';
+
+const props = defineProps<{
+    variant: 'underline' | 'filled',
+    orientation: 'horizontal' | 'vertical'
+}>();
+
+provide('tabVariant', props.variant);
+provide('tabOrientation', props.orientation);
+
+const klass = computed(() => {
+    return {
+        'tablist--vertical': props.orientation === 'vertical',
+        'tablist--horizontal': props.orientation === 'horizontal',
+        'tablist--underline': props.variant === 'underline',
+        'tablist--filled': props.variant === 'filled',
+    }
+})
+</script>
+
+<template>
+    <div
+        role="tablist"
+        class="tablist"
+        :class="klass"
+        :aria-orientation="props.orientation"
+    >
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="css" scoped>
+.tablist {
+    --widget-tablist-flex-direction: ;
+    --widget-tablist-gap: ;
+
+    display: flex;
+    flex-direction: var(--widget-tablist-flex-direction);
+    gap: var(--widget-tablist-gap);
+}
+
+.tablist--vertical {
+    --widget-tablist-flex-direction: column;
+}
+
+.tablist--horizontal {
+    --widget-tablist-flex-direction: row;
+}
+
+.tablist--vertical.tablist--underline {
+    --widget-tablist-gap: 0.75rem;
+}
+
+.tablist--vertical.tablist--filled {
+    --widget-tablist-gap: 0.5rem;
+}
+
+.tablist--horizontal.tablist--underline {
+    --widget-tablist-gap: 1rem;
+}
+
+.tablist--horizontal.tablist--filled {
+    --widget-tablist-gap: 0.5rem;
+}
+</style>
