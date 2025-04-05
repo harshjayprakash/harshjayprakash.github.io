dd9e9cec6e3afbde252a7d1c0430a7a18da846c3
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-05 21:50:43 +0100

    feat(surface): add spacing between slots

--- src/layouts/SurfaceLayout.vue
+++ src/layouts/SurfaceLayout.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import SpacerWidget from '@/components/SpacerWidget.vue';
 </script>
 
 <template>
     <div class="surface">
         <slot name="header"></slot>
+        <SpacerWidget :spacing="1.5" orientation="vertical" />
         <slot></slot>
+        <SpacerWidget :spacing="1.5" orientation="vertical" />
         <slot name="footer"></slot>
     </div>
 </template>
