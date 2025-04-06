b72e263263905ec21daf95525eff0eac2db96c4a
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-06 21:11:07 +0100

    refactor: remove spacer, change to flexbox

--- src/layouts/SurfaceLayout.vue
+++ src/layouts/SurfaceLayout.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
-import SpacerWidget from '@/components/SpacerWidget.vue';
 </script>
 
 <template>
     <div class="surface">
         <slot name="header"></slot>
-        <SpacerWidget :spacing="1.5" orientation="vertical" />
         <slot></slot>
-        <SpacerWidget :spacing="1.5" orientation="vertical" />
         <slot name="footer"></slot>
     </div>
 </template>
@@ -17,5 +14,8 @@ import SpacerWidget from '@/components/SpacerWidget.vue';
     max-width: 60rem;
     padding-inline: 2rem;
     margin-inline: auto;
+    display: flex;
+    flex-direction: column;
+    gap: 1.25rem;
 }
 </style>
