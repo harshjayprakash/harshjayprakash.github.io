019465c89eb4a54c2c7a8355d5938f08c7c4e5e6
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-06 20:36:03 +0100

    feat(data): add services data

---
+++ src/data/services.ts
@@ -0,0 +1,31 @@
+import type { OfferedServices } from "@/types/OfferedService";
+
+const services: OfferedServices[] = [
+    {
+        title: 'Front-End Web Development',
+        details: 'Crafting responsive websites using modern front-end technologies to create seamless user experiences.',
+        technologies: [
+            'HTML', 'CSS', 'TypeScript', 'Angular', 'Vue'
+        ]
+    },
+    {
+        title: 'Traditional Application Development',
+        details: 'Building robust, scalable applications tailored to the given requirements.',
+        technologies: [
+            'C', 'Python', 'Java', 'Visual Basic .NET', 'Pascal'
+        ]
+    },
+    {
+        title: 'UI/UX',
+        details: 'Designing interfaces that balance aesthetics with functionality',
+        technologies: [
+            'Figma', 'Pen+Paper'
+        ]
+    }
+];
+
+const getServices = () => {
+    return { services };
+}
+
+export default getServices;
