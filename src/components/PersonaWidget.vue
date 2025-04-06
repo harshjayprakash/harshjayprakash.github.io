cf091ce3d52d99d2f7f44c0e89781d7760cb3da7
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-06 20:34:54 +0100

    feat(persona): add persona component

---
+++ src/components/PersonaWidget.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+const props = defineProps<{
+    avatar: string,
+    name: string,
+    title: string,
+    status?: string
+}>();
+</script>
+
+<template>
+    <div role="status" class="persona">
+        <img class="avatar" :src="props.avatar" alt="profile picture" />
+        <section class="information" aria-label="Person Information">
+            <span class="name">{{ props.name }}</span>
+            <span class="title">{{ props.title }}</span>
+            <span v-if="status != undefined" class="status">{{ props.status }}</span>
+        </section>
+    </div>
+</template>
+
+<style lang="css" scoped>
+.persona {
+    display: flex;
+    gap: 1rem;
+}
+.avatar {
+    width: 2.5rem;
+    height: 2.5rem;
+    border-radius: 50%;
+}
+
+.information {
+    display: flex;
+    flex-direction: column;
+}
+
+.title, .status {
+    color: var(--colour-text-faded);
+    font-size: small;
+}
+</style>
