5583bb1fd441f63dd922dbcd01c8c10abf24400d
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-09 22:02:08 +0100

    feat(alpha-view): update to include dummy content

--- src/views/alpha/AlphaView.vue
+++ src/views/alpha/AlphaView.vue
@@ -1,8 +1,59 @@
 <script setup lang="ts">
+import SpacerWidget from '@/components/SpacerWidget.vue';
+import ArticleLayout from '@/layouts/ArticleLayout.vue';
+
 </script>
 
 <template>
     <article class="alpha" aria-label="Test">
-        &alpha; &beta; &gamma;
+        <h1>&alpha;&beta;&gamma;</h1>
+        <SpacerWidget orientation="vertical" :spacing="3" />
+        <ArticleLayout>
+            <h1>Title</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non dapibus lorem, vitae porta nibh.
+                Nunc semper tempor dolor eget ultrices. Donec in felis id magna accumsan ultrices in eu nibh. Vivamus
+                ullamcorper a elit sed mollis. Proin finibus sit amet odio eu tincidunt. Morbi lobortis enim et mauris
+                pretium, nec hendrerit quam iaculis. Integer mollis pellentesque viverra. In vel neque ac velit aliquam
+                porta vel non nulla. In semper mi id congue dignissim. Etiam eget purus tortor. Sed posuere libero at
+                lorem egestas hendrerit. Donec dictum convallis tempus. Nam luctus faucibus mauris, eu lacinia nisl
+                placerat eget. Integer accumsan urna ac ipsum lacinia porttitor.
+            </p>
+            <p>
+                Donec sed purus non ex dapibus luctus. Donec aliquam vulputate sagittis. Ut eget facilisis quam, non
+                aliquam quam. Praesent varius diam urna, nec ultricies nibh malesuada eu. Fusce aliquam in urna interdum
+                egestas. Ut facilisis ultrices lacinia. Nulla quam felis, scelerisque scelerisque tincidunt
+                sollicitudin, posuere quis mauris. Pellentesque consequat ipsum non sem porttitor pharetra. Maecenas
+                eget varius elit. Sed faucibus placerat consectetur. Curabitur sollicitudin volutpat varius.
+            </p>
+            <p>
+                Aliquam eget lorem sagittis, lobortis ante ac, hendrerit felis. Duis porttitor, felis vitae scelerisque
+                tempus, sapien ipsum vehicula magna, sed malesuada lorem justo eu augue. Proin porttitor quam eget diam
+                cursus varius. Proin scelerisque leo vitae scelerisque sodales. Nunc ut tincidunt quam. Interdum et
+                malesuada fames ac ante ipsum primis in faucibus. Integer et tortor eget massa suscipit egestas et a
+                ante.
+            </p>
+            <p>
+                Mauris convallis eget urna nec vehicula. Proin mattis justo lacus, ut pharetra quam hendrerit quis.
+                Donec congue tellus diam, porta pellentesque libero semper vel. Nam nunc dui, venenatis sed risus sit
+                amet, cursus maximus quam. Vestibulum dignissim ultrices lacus, ut ultricies orci mattis laoreet.
+                Praesent consectetur turpis ultricies, congue risus at, sollicitudin ex. Donec rhoncus aliquet nibh
+                ullamcorper tempor. In maximus sapien lacus, eget tristique nisi condimentum vitae. Aenean ullamcorper
+                cursus bibendum. Suspendisse potenti.
+            </p>
+            <p>
+                Nullam quis dignissim lorem, in finibus arcu. Sed blandit vestibulum nunc, sit amet semper dui
+                ullamcorper nec. Donec convallis leo quis porta molestie. Etiam tempus eget nunc non tempus. Nullam
+                fringilla enim sit amet suscipit suscipit. Vestibulum sit amet condimentum erat. Vestibulum ante ipsum
+                primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse potenti. Curabitur ut
+                interdum dui. Nullam tincidunt risus dictum luctus scelerisque. Aenean dapibus, sapien eget dignissim
+                ultricies, tellus ipsum suscipit neque, in rutrum elit ligula quis risus. Quisque quis massa venenatis,
+                hendrerit est in, convallis velit. Vivamus posuere neque tempus venenatis cursus. Pellentesque finibus
+                tortor et mauris tempus convallis. Vivamus lacinia lobortis tortor, sollicitudin semper urna efficitur
+                non. Cras et libero vel nulla pretium bibendum.
+            </p>
+        </ArticleLayout>
     </article>
 </template>
+
+<style lang="css" scoped></style>
