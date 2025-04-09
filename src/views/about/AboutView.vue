14c3cc1b7006038f7e3efb409eb822abb6333a88
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-09 22:02:33 +0100

    feat(about-view): add title, add quals section

--- src/views/about/AboutView.vue
+++ src/views/about/AboutView.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import AboutEducationSection from './AboutEducationSection.vue';
+
 </script>
 
 <template>
     <article class="about" aria-label="About">
-        About Page
+        <h1>About</h1>
+        <AboutEducationSection />
     </article>
 </template>
