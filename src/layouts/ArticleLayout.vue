029d964903d87b1b04a2923d180770c16ed3b92b
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-09 22:00:39 +0100

    feat(article-layout): add article layout component

---
+++ src/layouts/ArticleLayout.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <article class="markdown-style">
+        <slot></slot>
+    </article>
+</template>
+
+<style lang="css" scoped>
+.markdown-style {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    max-width: 50ch;
+    margin-inline: auto;
+}
+</style>
