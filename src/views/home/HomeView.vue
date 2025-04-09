b3d6ce172f69a9d7a70f583863249f9368d748e2
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-09 22:01:27 +0100

    feat(home-view): add sections

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts">
+import HomeContactSection from '@/views/home/HomeContactSection.vue';
+import HomeHeroSection from '@/views/home/HomeHeroSection.vue';
+import HomeProjectSection from '@/views/home/HomeProjectSection.vue';
+import HomeServiceSection from '@/views/home/HomeServiceSection.vue';
 </script>
 
 <template>
-    <article class="home" aria-label="Home View">
-        <h1>Home Page</h1>
+    <article class="home" aria-label="Home">
+        <HomeHeroSection />
+        <HomeServiceSection />
+        <HomeProjectSection />
+        <HomeContactSection />
     </article>
 </template>
+
+<style lang="css" scoped>
+.home {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+}
+</style>
