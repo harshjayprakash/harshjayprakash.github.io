06231dabef771b33c91c1e802ec255c0fc20b5c8
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-22 18:01:58 +0100

    feat(main): add router view

--- src/MainComponent.vue
+++ src/MainComponent.vue
@@ -1,6 +1,10 @@
 <script lang="ts" setup>
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
     <div>MainComponent</div>
+    <RouterView>
+        no content available on this route.
+    </RouterView>
 </template>
