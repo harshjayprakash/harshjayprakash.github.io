7b33e89b8478b0a5296882567fb057c523a404ca
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-22 18:01:30 +0100

    feat(main): use router

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import '@fontsource-variable/inter/opsz';
 import '@/assets/style/main.css';
 
 import MainComponent from '@/MainComponent.vue';
+import router from '@/router';
 
 const app = createApp(MainComponent);
+app.use(router);
 app.mount('#app');
