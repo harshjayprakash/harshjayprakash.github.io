6a07ff320227828900b6b2b6728c00e78729382c
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-22 18:33:52 +0100

    feat(home): add home sections

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -1,8 +1,16 @@
 <script lang="ts" setup>
+import HomeBioSection from './HomeBioSection.vue';
+import HomeContactSection from './HomeContactSection.vue';
+import HomeIntroSection from './HomeIntroSection.vue';
+import HomeWorkSection from './HomeWorkSection.vue';
 </script>
 
 <template>
     <article aria-label="Home Page" class="home">
         <h1 id="home">Home Page</h1>
+        <HomeIntroSection />
+        <HomeBioSection />
+        <HomeWorkSection />
+        <HomeContactSection />
     </article>
 </template>
