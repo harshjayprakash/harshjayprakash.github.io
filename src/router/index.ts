7f340db1aa6be332223b09e10c3a3e3f6315bc05
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-22 18:05:51 +0100

    feat(router): add home page route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import HomePage from '@/pages/home/HomePage.vue';
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [{
         path: '/',
         redirect: '/home'
+    }, {
+        path: '/home',
+        name: 'Home',
+        component: HomePage
     }]
 });
 
