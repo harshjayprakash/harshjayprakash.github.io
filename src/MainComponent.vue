a516b91443855b3596fca343b86fc8f5f37c93be
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-24 23:04:11 +0100

    feat(main): add footer

--- src/MainComponent.vue
+++ src/MainComponent.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 import { RouterView } from 'vue-router';
+import TheFooter from '@/components/TheFooter.vue';
 </script>
 
 <template>
     <main id="main">
         <RouterView />
     </main>
+    <TheFooter />
 </template>
