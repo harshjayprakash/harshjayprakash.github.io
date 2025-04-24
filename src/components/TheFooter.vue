0f3a5fe259a37f61e3d3429fb6706bb67d03d9ec
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-24 23:03:58 +0100

    feat(footer): add footer component

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <footer class="footer">
+        <small>Version 5.0 &mdash; 25H1F, 24.04.2025. Under Construction</small>
+        <small>Copyright &copy; 2025 Harsh Jayprakash. Handcrafted with Vue.</small>
+    </footer>
+</template>
+
+<style lang="css" scoped>
+.footer {
+    display: flex;
+    flex-direction: column;
+}
+</style>
