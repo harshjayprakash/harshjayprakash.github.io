a63ddd636ab363d92d020dac2d2f93f19909dea5
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-24 23:01:12 +0100

    feat: re-add data

---
+++ src/data/altText.ts
@@ -0,0 +1,35 @@
+import type { AltText } from "@/types/AltText";
+
+const altText: AltText[] = [{
+    name: 'ppw-screenshot.png',
+    text: 'A web browser window displaying the hero section of the website.'
+}, {
+    name: 'xbk-screenshot.png',
+    text: 'An application showing a blocky drawing.'
+}, {
+    name: 'aap-screenshot.png',
+    text: 'A notepad-like window displaying some code.'
+}, {
+    name: 'wpq-screenshot.png',
+    text: 'A window showing a page titled \'current quotes for this order\'.'
+}, {
+    name: 'lls-screenshot.png',
+    text: 'A window with sidebar navigation and statistics display under the \'Home\' title'
+}, {
+    name: 'dwf-screenshot.png',
+    text: 'A web browser window displaying a page titled \'Find Dog Walkers\'.'
+}, {
+    name: 'ccs-screenshot.png',
+    text: 'A terminal displaying the resuling text.'
+}];
+
+const findAltTextFromName = (name: string) => {
+    const alt = altText.find(text => text.name === name);
+    return (alt == undefined) ? '' : alt.text;
+}
+
+const getAltText = () => {
+    return { altText, findAltTextFromName };
+}
+
+export default getAltText;
---
+++ src/data/metadata.ts
@@ -0,0 +1,17 @@
+const copyrightYear = 2025;
+const epochSemanticVersion = '5.0';
+const calendarVersion = '25H1F';
+const buildStatus = 'ALPHA';
+const buildDate = '13.04.2025';
+
+const getMetaData = () => {
+    return {
+        copyrightYear,
+        epochSemanticVersion,
+        calendarVersion,
+        buildStatus,
+        buildDate
+    };
+}
+
+export default getMetaData;
