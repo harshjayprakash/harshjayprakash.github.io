42f48dbecad6e5cb4867efd383e7273774dec715
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-26 22:37:18 +0100

    feat(not-found): add not found page

---
+++ src/pages/not-found/NotFoundPage.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <article aria-label="Page Not Found" class="not-found">
+        <h1>Seems you've wondered off the map.</h1>
+        <RouterLink to="/home">Back to Home</RouterLink>
+    </article>
+</template>
+
+<style lang="css" scoped>
+.not-found {
+    display: flex;
+    flex-direction: column;
+    height: 90vh;
+    place-content: center;
+    place-items: center;
+    gap: 5rem;
+}
+</style>
