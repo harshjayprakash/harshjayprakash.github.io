916c4695d59c3b8006b73378eb4b830fb93f7f5e
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-26 19:25:03 +0100

    feat(badge): add badge component

---
+++ src/components/TheBadge.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+defineOptions({ inheritAttrs: false });
+
+const { variant } = defineProps<{
+    variant: 'ghost' | 'outline' | 'tint' | 'filled'
+}>();
+
+const styleKlass = computed(() => {
+    return {
+        'badge--ghost': variant === 'ghost',
+        'badge--outline': variant === 'outline',
+        'badge--tint': variant === 'tint',
+        'badge--filled': variant === 'filled'
+    }
+});
+</script>
+
+<template>
+    <div
+        role="status"
+        class="badge"
+        :class="styleKlass"
+        v-bind="$attrs"
+        v-on="$attrs"
+    >
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="css" scoped>
+
+</style>
