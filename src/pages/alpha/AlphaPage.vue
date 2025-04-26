cf1a08d3c551cbd4c1ccf987681856b8e8b2e801
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-26 22:38:34 +0100

    feat(alpha): add alpha testing page

---
+++ src/pages/alpha/AlphaPage.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <article aria-label="Testing Page, Not for Public Use" class="alpha">
+        <h1>&alpha;&beta;&gamma;</h1>
+    </article>
+</template>
