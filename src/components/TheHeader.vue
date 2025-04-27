dabdf1a482c9491df8bf78bfc06feb11e3a0ed5d
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-27 18:51:23 +0100

    feat(header): re-implement header content and styles

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,8 +1,30 @@
 <script lang="ts" setup>
+import getMetaData from '@/data/metadata';
+import TheBadge from './TheBadge.vue';
+const { buildStatus } = getMetaData();
 </script>
 
 <template>
-    <header aria-label="Site Header">
-
+    <header aria-label="Site Header" class="header">
+        <span>Harsh.</span>
+        <TheBadge variant="outline" >
+            {{ buildStatus }}
+        </TheBadge>
     </header>
 </template>
+
+<style lang="css" scoped>
+.header {
+    background-color: var(--colour-bk-secondary);
+    position: sticky;
+    top: 1rem;
+    display: flex;
+    gap: 1rem;
+    padding: 1rem;
+    max-width: min-content;
+    border: 1px solid var(--colour-outline-faded);
+    box-shadow: var(--shadow-lg);
+    place-items: center;
+    z-index: 10;
+}
+</style>
