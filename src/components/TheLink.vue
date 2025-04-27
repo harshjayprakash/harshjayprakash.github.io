2ed19ce630cf4b85da6338f72bd1f0b34d057081
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-27 15:31:20 +0100

    feat(link): add link component

---
+++ src/components/TheLink.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const { variant, to, exact, newWindow } = defineProps<{
+    variant: 'internal' | 'external',
+    to: string,
+    exact: boolean,
+    newWindow: boolean
+}>();
+
+const rel = () => (newWindow) ? 'noopener noreferrer' : '';
+const target = () => (newWindow) ? '_blank' : '';
+</script>
+
+<template>
+    <a v-if="variant === 'external'" :href="to" :rel :target class="link">
+        <slot></slot>
+    </a>
+    <RouterLink v-if="variant === 'internal'" :to :exact class="link">
+        <slot></slot>
+    </RouterLink>
+</template>
+
+<style lang="css" scoped>
+</style>
