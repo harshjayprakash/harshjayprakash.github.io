553ee80a4d7c37651dc4dfec860ab10f34916e5f
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-27 12:17:22 +0100

    feat(accordian): add accordian component

---
+++ src/components/TheAccordian.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <div role="group">
+        <details>
+            <summary>
+                <slot name="heading"></slot>
+            </summary>
+            <slot name="content"></slot>
+        </details>
+    </div>
+</template>
