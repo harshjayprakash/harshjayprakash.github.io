c11f7f56b06ac2febdd07254d76a1f25d6de8d9f
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-27 13:24:23 +0100

    feat(accordion): update accordion style

--- src/components/TheAccordion.vue
+++ src/components/TheAccordion.vue
@@ -11,3 +11,36 @@
         </div>
     </details>
 </template>
+
+<style lang="css" scoped>
+.accordion-content {
+    padding-inline: 1rem;
+    border-inline-start: 0.2rem solid transparent;
+}
+
+.accordion-header {
+    list-style-type: '\002B   ';
+    cursor: pointer;
+}
+
+.accordion-header > * {
+    display: inline;
+}
+
+.accordion-content[open] > .accordion-header {
+    list-style-type: '\2212   ';
+}
+
+.accordion-header::marker {
+    font-weight: 500;
+    color: var(--colour-600H);
+}
+
+.accordion-content[open] {
+    border-inline-start: 0.2rem solid var(--colour-600H);
+}
+
+.accordion-content-details {
+    padding-block: 0.5rem;
+}
+</style>
