ed7a31efb4ed4676bb578041b07444fcfc535fc2
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-27 14:43:31 +0100

    feat(alpha): add test container

--- src/pages/alpha/AlphaPage.vue
+++ src/pages/alpha/AlphaPage.vue
@@ -4,5 +4,14 @@
 <template>
     <article aria-label="Testing Page, Not for Public Use" class="alpha">
         <h1>&alpha;&beta;&gamma;</h1>
+        <div class="test-container">
+
+        </div>
     </article>
 </template>
+
+<style lang="css" scoped>
+.test-container {
+    padding-block: 5rem;
+}
+</style>
