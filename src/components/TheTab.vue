cb88a54deab2afcdb4991fbbf4a7fd7b73ee606a
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-27 11:14:52 +0100

    feat(tab): add tab based components

---
+++ src/components/TheTab.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <button role="tab">
+        <slot></slot>
+    </button>
+</template>
---
+++ src/components/TheTabList.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <div role="tablist">
+        <slot></slot>
+    </div>
+</template>
