3b57306f5174ad336aaa4cfe45fe03f6cd35b96f
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-27 22:00:29 +0100

    refactor(composable): remove generate function, move logic to onmounted

--- src/composables/useDotParticles.ts
+++ src/composables/useDotParticles.ts
@@ -1,19 +1,18 @@
 import type { Position } from "@/types/Position"
-import { readonly, ref } from "vue"
+import { onMounted, readonly, ref } from "vue"
 
-const useDotParticles = () => {
+const useDotParticles = (dotCount: number) => {
     const dots = ref<Position[]>([]);
 
-    const generateDots = (dotCount: number) => {
-        dots.value.splice(0);
+    onMounted(() => {
         for (let idx = 0; idx < dotCount; idx++) {
             const x = Math.floor(Math.random()*100);
             const y = Math.floor(Math.random()*100);
             dots.value.push({ x, y });
         }
-    }
+    });
 
-    return { dots: readonly(dots), generateDots };
+    return { dots: readonly(dots) };
 }
 
 export default useDotParticles;
