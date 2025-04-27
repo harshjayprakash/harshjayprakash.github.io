a9b4219ad5194127d19ee1f667a81fc89df97a3e
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-27 18:45:07 +0100

    feat(main): add header

--- src/MainComponent.vue
+++ src/MainComponent.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 import { RouterView } from 'vue-router';
 import TheFooter from '@/components/TheFooter.vue';
+import TheHeader from '@/components/TheHeader.vue';
 </script>
 
 <template>
+    <TheHeader />
     <main id="main">
         <RouterView />
     </main>
