4fddc4056089f09b1ee5cbb0fdd265eb1d8f5533
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-27 15:32:14 +0100

    feat(badge): add badge styling

--- src/components/TheBadge.vue
+++ src/components/TheBadge.vue
@@ -30,5 +30,42 @@ const styleKlass = computed(() => {
 </template>
 
 <style lang="css" scoped>
+.badge {
+    --widget-badge-text: ;
+    --widget-badge-bk: ;
+    --widget-badge-outline: ;
 
+    display: inline;
+    width: fit-content;
+    font-size: smaller;
+    background-color: var(--widget-badge-bk);
+    color: var(--widget-badge-text);
+    border: 0.1rem solid var(--widget-badge-outline);
+    padding-inline: 0.25rem;
+    padding-block: 0.1rem;
+}
+
+.badge.badge--ghost {
+    --widget-badge-text: var(--colour-text-highlight);
+    --widget-badge-bk: inherit;
+    --widget-badge-outline: transparent;
+}
+
+.badge.badge--outline {
+    --widget-badge-text: var(--colour-text-highlight);
+    --widget-badge-bk: inherit;
+    --widget-badge-outline: var(--colour-outline-highlight);
+}
+
+.badge.badge--tint {
+    --widget-badge-text: var(--colour-text-highlight);
+    --widget-badge-bk: var(--colour-bk-highlight-subtle);
+    --widget-badge-outline: var(--colour-outline-highlight-subtle);
+}
+
+.badge.badge--filled {
+    --widget-badge-text: var(--colour-text-inverted);
+    --widget-badge-bk: var(--colour-bk-highlight);
+    --widget-badge-outline: var(--colour-outline-highlight);
+}
 </style>
