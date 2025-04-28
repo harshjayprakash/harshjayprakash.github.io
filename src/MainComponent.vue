8d39e6022f2c49cd4f5c08cdd9081ef6a1f96716
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-28 22:29:15 +0100

    feat(main): add skip to main content link

--- src/MainComponent.vue
+++ src/MainComponent.vue
@@ -2,9 +2,13 @@
 import { RouterView } from 'vue-router';
 import TheFooter from '@/components/TheFooter.vue';
 import TheHeader from '@/components/TheHeader.vue';
+import TheLink from './components/TheLink.vue';
 </script>
 
 <template>
+    <TheLink variant="external" to="#main" :new-window="false" exact>
+        Skip to main content
+    </TheLink>
     <TheHeader />
     <main id="main">
         <RouterView />
