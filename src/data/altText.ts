eab8bdd354926f2fab017e981329daa74d679acd
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-04-30 20:23:09 +0100

    fix: image object name

--- src/data/altText.ts
+++ src/data/altText.ts
@@ -1,31 +1,31 @@
 import type { AltText } from "@/types/AltText";
 
 const altText: AltText[] = [{
-    name: 'ppw-screenshot.png',
+    name: 'ppw-preview.png',
     text: 'A web browser window displaying the hero section of the website.'
 }, {
-    name: 'xbk-screenshot.png',
+    name: 'xbk-preview.png',
     text: 'An application showing a blocky drawing.'
 }, {
-    name: 'aap-screenshot.png',
+    name: 'aap-preview.png',
     text: 'A notepad-like window displaying some code.'
 }, {
-    name: 'wpq-screenshot.png',
+    name: 'wpq-preview.png',
     text: 'A window showing a page titled \'current quotes for this order\'.'
 }, {
-    name: 'lls-screenshot.png',
+    name: 'lls-preview.png',
     text: 'A window with sidebar navigation and statistics display under the \'Home\' title'
 }, {
-    name: 'dwf-screenshot.png',
+    name: 'dwf-preview.png',
     text: 'A web browser window displaying a page titled \'Find Dog Walkers\'.'
 }, {
-    name: 'ccs-screenshot.png',
+    name: 'ccs-preview.png',
     text: 'A terminal displaying the resulting text.'
 }];
 
 const findAltTextFromName = (name: string) => {
     const alt = altText.find(text => text.name === name);
-    return (alt == undefined) ? '' : alt.text;
+    return (alt === undefined) ? '' : alt.text;
 }
 
 const getAltText = () => {
