d8ac3524e476f9a33175df68972111d0475847db
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-06-23 13:43:59 +0100

    feat: update imports

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from "vue";
-import MainComponent from "./MainComponent.vue";
+import { createApp } from 'vue';
+
+import '@fontsource-variable/inter';
+import '@/assets/css/main.css';
+
+import MainComponent from './MainComponent.vue';
 
 const app = createApp(MainComponent);
 app.mount('#app');
