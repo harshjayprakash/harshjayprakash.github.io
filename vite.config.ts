a3d7b27de6d63b591a2948d55ef04b5f9cef0da3
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-06-23 13:34:34 +0100

    build: optimise for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { fileURLToPath, URL } from 'node:url';
+import { resolve } from 'node:path';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 export default defineConfig({
+    base: '/',
     plugins: [
         vue(),
         vueDevTools(),
@@ -14,4 +16,10 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url))
         },
     },
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: resolve(__dirname, 'index.html')
+        }
+    }
 });
