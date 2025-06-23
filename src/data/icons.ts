11db59bf0ae593d4208d63cca1a17165c5bd90aa
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-06-23 17:13:43 +0100

    feat(data): add icon translation

---
+++ src/data/icons.ts
@@ -0,0 +1,12 @@
+const navIcons = {
+    'Home': 'home',
+    'Projects': 'workspaces',
+    'CV': 'description',
+    'About': 'person'
+}
+
+const getIcons = () => {
+    return { navIcons };
+}
+
+export default getIcons;
