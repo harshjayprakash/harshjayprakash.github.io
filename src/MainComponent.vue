8282c3e7377d443d5a14d802a9f7383b5aa60c4b
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-06-23 22:57:23 +0100

    feat: add content to app main

--- src/MainComponent.vue
+++ src/MainComponent.vue
@@ -1,6 +1,19 @@
 <script setup lang="ts">
+import { RouterView } from 'vue-router';
+import TheHeader from './components/TheHeader.vue';
+import TheFooter from './components/TheFooter.vue';
 </script>
 
 <template>
-    Hello.
+    <TheHeader />
+    <main class="content-wrapper">
+        <RouterView />
+    </main>
+    <TheFooter />
 </template>
+
+<style lang="css" scoped>
+.content-wrapper {
+    padding-block: 3rem 2rem;
+}
+</style>
