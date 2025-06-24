def88fcc920d5e3c9af1f218d837b82ccebd63fb
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-06-24 22:49:31 +0100

    feat: add home section components

---
+++ src/pages/home/HomeCTASection.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <section>
+        <h2>Call to Action</h2>
+    </section>
+</template>
---
+++ src/pages/home/HomeSkillsSection.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <section>
+        <h2>Skills.</h2>
+    </section>
+</template>
