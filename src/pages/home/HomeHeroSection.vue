adaca4cfe5500fbf12681568c36e5a01786712ee
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-06-24 16:28:05 +0100

    feat: update hero section

--- src/pages/home/HomeHeroSection.vue
+++ src/pages/home/HomeHeroSection.vue
@@ -1,40 +1,101 @@
 <script setup lang="ts">
+import TheLink from '@/components/TheLink.vue';
+import ThePersona from '@/components/ThePersona.vue';
+import useDotParticles from '@/composables/useDotParticles';
+import useMobileDetector from '@/composables/useMobileDetector';
 import getAvatar from '@/data/avatar';
+import getSizeConstraints from '@/data/sizing';
 
 const { avatar } = getAvatar();
+const { mobileBreakpoint } = getSizeConstraints();
+const { isMobile } = useMobileDetector(mobileBreakpoint);
+
+const _dotCount = isMobile.value ? 45 : 60;
+const { dots } = useDotParticles(_dotCount);
 </script>
 
 <template>
     <section aria-label="Home Page Hero Section" class="home-hero">
-        <img :src="avatar" alt="Profile Picture" class="avatar" />
+        <ThePersona :avatar name="Harsh Jayprakash"
+            status="Looking for experience/work." />
         <h1>
-            I'm Harsh, a Software Developer<br />based in Southampton, UK.
+            I'm Harsh &mdash; a <span class="highlight">Software Developer</span>,
+            crafting digital experiences that feel intentional.
         </h1>
-        <p>Crafting functional and thoughtful digital experience.</p>
-        <p></p>
-        <div class="rect">
-
+        <ul class="social-list">
+            <li class="social-item"><TheLink
+                variant="external"
+                to="mailto:harshjayprakash@outlook.com"
+                new-window>Email</TheLink></li> //
+            <li class="social-item"><TheLink
+                variant="external"
+                to="https://www.linkedin.com/in/harshjayprakash7/"
+                new-window>LinkedIn</TheLink></li> //
+            <li class="social-item"><TheLink
+                variant="external"
+                to="https://github.com/harshjayprakash"
+                new-window>GitHub</TheLink></li>
+        </ul>
+        <div class="dots-container">
+            <span class="explore-arrow">&downarrow; scroll to explore</span>
+            <div
+                v-for="(dot, idx) in dots"
+                :key="`${idx}${dot.x}${dot.y}`"
+                aria-hidden="true"
+                class="dot"
+                :style="{'left':`${dot.x}%`, 'top':`${dot.y}%`}"
+                >
+            </div>
         </div>
     </section>
 </template>
 
 <style lang="css" scoped>
 .home-hero {
-    min-height: 90vh;
+    min-height: 80dvh;
     display: flex;
     flex-direction: column;
     gap: 2rem;
 }
 
-.avatar {
-    width: 4rem;
-    height: 4rem;
-    border-radius: 50%;
+.explore-arrow {
+    padding-inline-end: 1rem;
+    padding-block: 0.75rem;
+    border-radius: 0 0 5px 0;
+    background-color: var(--colour-bk-primary);
+    color: var(--colour-text-faded);
 }
 
-.rect {
-    border-radius: 5px;
-    height: 30vh;
-    background: lightcyan;
+.highlight {
+    background-color: var(--colour-bk-highlight-subtle);
+    color: var(--colour-text-highlight);
+}
+
+.social-list {
+    list-style: none;
+    padding: 0;
+    display: flex;
+    gap: 1rem;
+}
+
+.dots-container {
+    overflow: clip;
+    flex-grow: 1;
+    background-color: var(--colour-bk-secondary);
+    border-radius: var(--rounded-default);
+    z-index: -1;
+}
+
+.dot {
+    opacity: 0.5;
+    position: relative;
+    background-color: var(--colour-bk-inverted);
+    height: 0.5rem;
+    width: 0.5rem;
+    z-index: -1;
+}
+
+.dot:hover {
+    background-color: blue;
 }
 </style>
