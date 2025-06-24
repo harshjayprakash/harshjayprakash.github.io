d7e8dcc9568c026135594c50ab197840f8182edd
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-06-24 22:49:18 +0100

    feat: add select projects section and spacing between sections

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
 import HomeHeroSection from './HomeHeroSection.vue';
+import HomeSelectProjectsSection from './HomeSelectProjectsSection.vue';
 </script>
 
 <template>
-    <article>
+    <article class="home" aria-label="Home Page">
         <HomeHeroSection />
+        <HomeSelectProjectsSection />
     </article>
 </template>
+
+<style lang="css" scoped>
+.home {
+    display: flex;
+    flex-direction: column;
+    gap: 7rem;
+}
+</style>
