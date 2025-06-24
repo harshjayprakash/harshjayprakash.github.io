279612b6f0bc6b7f9cb5b9aa386a80eabfb573f0
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-06-24 13:51:36 +0100

    feat: add go to home button

--- src/pages/notfound/NotFoundPage.vue
+++ src/pages/notfound/NotFoundPage.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
+import TheButton from '@/components/TheButton.vue';
 </script>
 
 <template>
     <article aria-label="Not Found" class="not-found">
         <h1>Seems you've wondered off the map.</h1>
+        <TheButton variant="internal" appearance="primary" to="/home" exact>
+            Go to Home
+        </TheButton>
     </article>
 </template>
 
 <style lang="css" scoped>
 .not-found {
     display: flex;
+    flex-direction: column;
+    gap: 5rem;
     height: 80vh;
     place-content: center;
     place-items: center;
