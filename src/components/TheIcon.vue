b36829336ed6d7a3490cd8efd3fbb2864ee72ad0
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-06-25 13:13:33 +0100

    feat: update to include filled icons.
    - add filled optional prop
    - move styles to component itself

--- src/components/TheIcon.vue
+++ src/components/TheIcon.vue
@@ -1,21 +1,43 @@
 <script setup lang="ts">
 import { defineProps } from 'vue';
 
-const { name } = defineProps<{
-    name: string
+const { name, filled = false } = defineProps<{
+    name: string,
+    filled?: boolean
 }>();
+
 </script>
 
 <template>
-    <span class="material-symbols-outlined icon-outline">
+    <span class="material-icon" :data-filled="filled">
         {{ name }}
     </span>
 </template>
 
-<style lang="css" scoped>
-.icon-outline {
+<style lang="css">
+.material-icon {
+    font-family: "Material Symbols Rounded Variable";
+    font-weight: normal;
+    font-style: normal;
+    font-size: 22px;
+    display: inline-block;
+    line-height: 1;
+    letter-spacing: normal;
+    white-space: nowrap;
+    direction: ltr;
+}
+
+.material-icon[data-filled='true'] {
     font-variation-settings:
         "FILL" 1,
+        "wght" 400,
+        "GRAD" 0,
+        "opsz" 22;
+}
+
+.material-icon[data-filled='false'] {
+    font-variation-settings:
+        "FILL" 0,
         "wght" 400,
         "GRAD" 0,
         "opsz" 22;
