f1f89db77d5f68b6eb214f7c90f39067fb59863d
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-06-26 11:31:30 +0100

    fix: platform filter type

--- src/types/project/ProjectPlatform.ts
+++ src/types/project/ProjectPlatform.ts
@@ -1,4 +1,4 @@
 type ProjectPlatform = 'CLI' | 'Desktop' | 'Web';
-type ProjectPlatformFilter = ProjectPlatform & 'All';
+type ProjectPlatformFilter = ProjectPlatform | 'All';
 
 export type { ProjectPlatform, ProjectPlatformFilter };
