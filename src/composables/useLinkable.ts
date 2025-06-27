e3b69559255bffc950bd41d8b90420eb16951f06
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-06-27 13:22:00 +0100

    feat: add use linkable composable

---
+++ src/composables/useLinkable.ts
@@ -0,0 +1,37 @@
+import { linkData } from "@/data/ui/link";
+import type { LinkableConfig, Linkable } from "@/types/ui/Linkable";
+import { computed } from "vue";
+
+const useLinkable = (vLink: Linkable, to: string, newWindow: boolean) => {
+    return computed<LinkableConfig>(() => {
+        switch (vLink) {
+        default:
+        case 'none':
+            return {
+                is: 'div',
+                href: undefined,
+                target: undefined,
+                rel: undefined,
+                to: undefined
+            };
+        case 'internal':
+            return {
+                is: 'RouterLink',
+                href: undefined,
+                target: undefined,
+                rel: undefined,
+                to: to
+            }
+        case 'external':
+            return {
+                is: 'a',
+                href: to,
+                target: linkData.newWindowOpenData.target(newWindow),
+                rel: linkData.newWindowOpenData.rel(newWindow),
+                to: undefined
+            }
+        }
+    });
+}
+
+export default useLinkable;
