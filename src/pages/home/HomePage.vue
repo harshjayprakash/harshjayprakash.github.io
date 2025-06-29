136bf17c530a064f59c57eb77bb3d7ae5ede1844
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-06-29 12:43:46 +0100

    feat(home): update home page
    - update components
    - remove scoped styles

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
+import HomeBioSection from './HomeBioSection.vue';
 import HomeHeroSection from './HomeHeroSection.vue';
-import HomeSelectProjectsSection from './HomeSelectProjectsSection.vue';
+import HomeSpotlightSection from './HomeSpotlightSection.vue';
 </script>
 
 <template>
     <article class="home" aria-label="Home Page">
         <HomeHeroSection />
-        <HomeSelectProjectsSection />
+        <HomeSpotlightSection />
+        <HomeBioSection />
     </article>
 </template>
 
-<style lang="css" scoped>
+<style lang="css">
 .home {
     display: flex;
     flex-direction: column;
