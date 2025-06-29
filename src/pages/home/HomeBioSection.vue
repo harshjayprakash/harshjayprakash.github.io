8a1fb36c5b73e1a63358d18aa2c1638d11a07346
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-06-29 12:43:12 +0100

    feat(home): add bio section

---
+++ src/pages/home/HomeBioSection.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import TheLink from '@/components/TheLink.vue';
+
+</script>
+
+<template>
+    <section class="home-bio" aria-label="Quick About">
+        <h2>What I bring to the table.</h2>
+        <div class="content">
+            <p>
+                I'm a recent graduate who's spent countless hours refining systems, helping
+                others troubleshoot, and exploring the craft of programming &ndash; not just
+                as a tool, but as a medium for clarity and expression.
+            </p>
+            <p>
+                I care deeply about cohesive design language, thoughtful architecture, and
+                building things that feel intention from the inside out.
+            </p>
+        </div>
+        <TheLink linkable="internal" to="/about">
+            Read more ->
+        </TheLink>
+    </section>
+</template>
+
+<style lang="css">
+.home-bio {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+}
+
+.home-bio .content {
+    max-width: 60ch;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+}
+</style>
