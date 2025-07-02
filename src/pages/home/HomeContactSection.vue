cbad6e9044e0800ffd5348e749ab23e68a95d2bf
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-07-02 11:25:33 +0100

    feat(home): update contact section
    - add content
    - add styles

--- src/pages/home/HomeContactSection.vue
+++ src/pages/home/HomeContactSection.vue
@@ -1,13 +1,44 @@
 <script setup lang="ts">
 import TheCard from '@/components/common/TheCard.vue';
-
+import TheLink from '@/components/common/TheLink.vue';
+import { personaData } from '@/data/profile/persona';
+import { socialData } from '@/data/profile/socials';
 </script>
 
 <template>
     <section class="home-contact" aria-label="Home Contact Section">
         <TheCard linkable="none" appearance="filled" elevate class="contact-card">
-            <h2>Looking to collaborate or say hello?</h2>
-            <p>I'm currently open to new opportunities.</p>
+            <div class="contact-title" role="group">
+                <h2>Say hello</h2>
+                <p class="faded-text">
+                    If you're working on something thoughtful &ndash; whether it's code,
+                    design, or something in between &ndash; I'd be happy to connect.
+                </p>
+            </div>
+            <ul class="link-list">
+                <li v-for="social in socialData.all" :key="social.social"
+                    class="link-item"
+                >
+                    <div role="group" class="link-wrapper">
+                        <TheLink linkable="external" :to="social.uri">
+                            {{ social.social }}
+                        </TheLink>
+                        <small class="faded-text">
+                            {{ social.description }}
+                        </small>
+                    </div>
+                </li>
+                <li class="link-item">
+                    <div role="group" class="link-wrapper">
+                        <TheLink linkable="external" :to="personaData.cv.uri">
+                            CV
+                        </TheLink>
+                        <small class="faded-text">
+                            {{ personaData.cv.description }}
+                        </small>
+                    </div>
+                </li>
+            </ul>
         </TheCard>
     </section>
 </template>
@@ -15,5 +46,40 @@ import TheCard from '@/components/common/TheCard.vue';
 <style lang="css">
 .home-contact .contact-card {
     border: 0.1rem solid var(--colour-bk-highlight);
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    gap: 1rem;
+}
+
+.home-contact .contact-card .contact-title {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+}
+
+.home-contact .link-list {
+    list-style: none;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+}
+
+.home-contact .link-list .link-wrapper {
+    display: flex;
+    flex-direction: column;
+}
+
+.home-contact .faded-text {
+    color: var(--colour-text-faded-less);
+}
+
+@media (min-width: 640px) {
+    .home-contact .contact-card {
+        border: 0.1rem solid var(--colour-bk-highlight);
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        gap: 3rem;
+    }
 }
 </style>
