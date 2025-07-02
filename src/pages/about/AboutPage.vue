cbbc8323a2b59099bdbebab84cf00eea8bac9b3b
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-07-02 11:26:42 +0100

    feat(about): update about page
    - remove card content
    - add quote
    - add social and cv links
    - add comment structure
    - update style

--- src/pages/about/AboutPage.vue
+++ src/pages/about/AboutPage.vue
@@ -1,41 +1,70 @@
 <script setup lang="ts">
-import ThePersona from '@/components/common/ThePersona.vue';
 import TheCard from '@/components/common/TheCard.vue';
-import { avatarData } from '@/data/profile/avatar';
+import TheDivider from '@/components/common/TheDivider.vue';
+import TheLink from '@/components/common/TheLink.vue';
+import ThePersona from '@/components/common/ThePersona.vue';
+import TheQuote from '@/components/common/TheQuote.vue';
+import { personaData } from '@/data/profile/persona';
+import { socialData } from '@/data/profile/socials';
 
-const avatar = avatarData.uri;
 </script>
 
 <template>
     <article aria-label="About" class="about">
         <h1>About</h1>
-        <ThePersona
-            :avatar
-            name="Harsh Jayprakash"
-            status="Looking for experience/work." />
-        <div role="group" class="about-info-cards">
-            <TheCard appearance="filled" linkable="none">
-                I am a computer science graduate from the University of Winchester, actively searching for a role.
-            </TheCard>
-            <TheCard appearance="filled" linkable="none">
-                I enjoy the craft of programming - focusing on making intentional
-                digital experiences. The art of programming is a way I connect with the
-                digital world, drawn to the challenge and creativity. I want to create an
-                impact, inspire and connect with others.
-            </TheCard>
-            <TheCard appearance="filled" linkable="none">
-                Moreover, designing diagrams has always been my favourite way to present
-                ideas. From selecting the right shapes and colours to adjusting font sizes
-                and spacing, I love ensuring every detail harmonises,  maintaining a
-                cohesive and unified visual language.
-            </TheCard>
-            <TheCard appearance="filled" linkable="none">
-                I have spent countless hours helping others, whether it is guiding them
-                through programming, troubleshooting the operating system, or a bit of
-                mathematics. Sharing knowledge and working collaboratively has always been
-                a rewarding part of my journey.
-            </TheCard>
-        </div>
+        <ThePersona :avatar="personaData.avatarUri" :name="personaData.name"
+            :status="personaData.status" />
+        <TheQuote>
+            I enjoy the quiet details &mdash; the ones that make things feel intentional.
+        </TheQuote>
+        <TheCard appearance="filled" linkable="none" class="social-link-card-wrapper">
+            <ul aria-label="Socials List and CV PDF Link" class="link-list">
+                <li v-for="social in socialData.all" :key="social.social">
+                    <TheLink linkable="external" :to="social.uri" new-window>
+                        {{ social.social }}
+                    </TheLink>
+                </li>
+            </ul>
+            <TheDivider orientation="vertical" :height="1.5" :thickness="0.1" />
+            <TheLink linkable="external" to="/documents/harsh_cv.pdf" new-window>
+                View CV
+        </TheLink>
+        </TheCard>
+        <section>
+            <h2>Titled</h2>
+
+        </section>
+
+        <!-- <section>
+            <h2>What I value</h2>
+            <ul>
+                <li>Clarity over Cleverness</li>
+                <li>Accessibility as a baseline, not a bonus</li>
+                <li>Systems that scale</li>
+                <li>Design that feels cohesive, not just consistant</li>
+            </ul>
+        </section> -->
+
+<!--
+        <section>
+            <h2>Introduction</h2>
+        </section>
+
+        <section>
+            <h2>A little bit about me.</h2>
+        </section>
+        <section>
+            <h2>What I Value</h2>
+        </section>
+        <section>
+            <h2>How I got Here</h2>
+        </section>
+        <section>
+            <h2>What I'm working On</h2>
+        </section>
+        <section>
+            <h2>Outside of Code</h2>
+        </section> -->
     </article>
 </template>
 
@@ -46,14 +75,36 @@ const avatar = avatarData.uri;
     gap: 1rem;
 }
 
-.about .about-info-cards {
-    display: grid;
+.about .social-link-card-wrapper {
+    max-width: fit-content;
+    display: flex;
     gap: 1rem;
+    flex-wrap: wrap;
+    /* flex-direction: column; */
 }
 
-@media (min-width: 768px) {
-    .about .about-info-cards {
-        grid-template-columns: repeat(3, 1fr);
-    }
+.about .link-list {
+    list-style: none;
+    padding: 0;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+    /* flex-direction: column; */
 }
+
+/* @media (min-width: 768px) {
+    .about .social-link-card-wrapper {
+        max-width: fit-content;
+        display: flex;
+        gap: 1rem;
+        flex-direction: row;
+    }
+
+    .about .link-list {
+        flex-direction: row;
+    }
+
+} */
+
+
 </style>
