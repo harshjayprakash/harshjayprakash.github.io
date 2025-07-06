d79b048b3844f3e82f3e965a37107d6dbfce6e07
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-07-06 12:30:59 +0100

    refactor: extract generate position method

--- src/composables/useDotParticles.ts
+++ src/composables/useDotParticles.ts
@@ -6,12 +6,19 @@ const useDotParticles = (dotCount: number) => {
     const dots = ref<BoxEntity[]>([]);
     const highlightDotIndex = ref(0);
 
+
+    const generatePosition_ = (endBound: number, scale: number) => {
+        return (Math.floor(Math.random()*endBound) / scale) * scale;
+    }
+
     onMounted(() => {
         highlightDotIndex.value = Math.floor(Math.random()*dotCount);
 
         for (let idx = 0; idx < dotCount; idx++) {
-            const x = Math.floor(Math.random()*120);
-            const y = Math.floor(Math.random()*120);
+            const x = generatePosition_(120, 10);
+            const y = generatePosition_(120, 10);
+
+
             dots.value.push({
                 positionX: x,
                 positionY: y,
