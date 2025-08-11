27ce85bc125101c820e138c11f53ee61f80fe067
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-08-11 17:09:00 +0100

    feat(footer): re-organise
    - add website status badge
    - add divider

--- src/layout/TheFooter.vue
+++ src/layout/TheFooter.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
+import TheBadge from '@/components/TheBadge.vue';
+import TheDivider from '@/components/TheDivider.vue';
 import TheLink from '@/components/TheLink.vue';
 import { metaData } from '@/data/meta';
 </script>
 
 <template>
     <footer class="footer" aria-label="Site Footer">
+        <TheDivider class="divider" orientation="horizontal" :margin="0.5"/>
         <small class="version-info">
-            Epoch Semantic Version {{ metaData.version.fullString() }}, {{ metaData.buildDate }}.
-            Under Construction.
+            <TheBadge appearance="tint" colour="highlight">
+                {{ metaData.buildStatus }}
+            </TheBadge>
+            Epoch Version {{ metaData.version.fullString() }},
+            {{ metaData.buildDate }}. Under Construction.
         </small>
         <small class="copyright">
-            &copy; {{ metaData.copyrightYear }} Harsh Jayprakash. Handcrafted with Vue.
+            <span>
+                &copy; {{ metaData.copyrightYear }} Harsh Jayprakash. Handcrafted with
+                Vue.
+            </span>
             <TheLink linkable="external" new-window
                 to="https://github.com/harshjayprakash/harshjayprakash.github.io"
             >
@@ -25,11 +34,16 @@ import { metaData } from '@/data/meta';
     display: flex;
     flex-direction: column;
     padding-block: 1rem;
+    gap: 0.15rem;
 }
 
 .footer .version-info,
 .footer .copyright {
     color: var(--colour-text-faded);
+}
+
+.footer .divider {
+    margin-block: 1rem;
 }
 
 @media (max-width: 639px) {
