afa322d777f97b541e9856a820f11d60c5a89e50
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-08-12 12:57:54 +0100

    feat(data): add about bullet points

--- src/data/profile/persona.ts
+++ src/data/profile/persona.ts
@@ -5,5 +5,13 @@ export const personaData = {
     cv: {
         uri: '/documents/harsh_cv.pdf',
         description: 'View or download my cv',
+    },
+    about: {
+        bullets: [
+            'First Class Honours in Computer Science, University of Winchester (2024)',
+            'I enjoy designing thoughful software - from desktop tools to web interfaces.',
+            'Currently seeking hands-on experience in development and UX-focused projects',
+            'Driven to make a meaningful contribution to the tech industry through curiosity, collaboration, and continuous learning.'
+        ]
     }
 }
